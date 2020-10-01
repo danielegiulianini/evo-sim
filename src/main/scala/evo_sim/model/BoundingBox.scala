698cb1d845ac05394e629c744ae9453dd0eb2985
Daniele Giulianini <danigiulio97@gmail.com>
2020-10-02 00:13:58 +0200

    add first prototyping implementations of bounding boxes

--- src/main/scala/evo_sim/model/BoundingBox.scala
+++ src/main/scala/evo_sim/model/BoundingBox.scala
@@ -12,3 +12,7 @@ trait BoundingBox {
   def position: (Int, Int)
   def shape : Shape
 }
+
+case class TriangularBoundingBox(override val shape: Triangle, override val position : (Int, Int)) extends BoundingBox
+case class RectangularBoundingBox(override val shape: Rectangle, override val position : (Int, Int)) extends BoundingBox
+case class CircularBoundingBox(override val shape: Rectangle, override val position : (Int, Int)) extends BoundingBox
