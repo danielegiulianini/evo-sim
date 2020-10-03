abe358c4b6e8d1546de508c81919e36a96a1f853
Andrea Vaienti <andrea.vaienti2@studio.unibo.it>
2020-10-03 15:50:43 +0200

    added movement strategies

---
+++ src/main/scala/evo_sim/model/MovingStrategies.scala
@@ -0,0 +1,37 @@
+package evo_sim.model
+
+import scala.math._
+
+object MovingStrategies {
+
+  def baseMovement(entity: Intelligent, entities: Set[Entity]): (Double, Double) = {
+    val chasedEntity = (entities - entity).minBy(distanceBetweenEntities(entity, _))
+    distanceBetweenEntities(entity, chasedEntity) < entity.fieldOfViewRadius match {
+      case true => chaseMovement(entity, chasedEntity)
+      case false => standardMovement(entity)
+    }
+  }
+
+  private def distanceBetweenEntities(a: Intelligent, b: Entity): Double = {
+    sqrt(pow(b.centerBoundingBox._1 - a.centerBoundingBox._1, 2) + pow(b.centerBoundingBox._2 - a.centerBoundingBox._2, 2))
+  }
+
+  private def standardMovement(entity: Intelligent): (Double, Double) = {
+    println("standardMovement")
+    val angle = new java.util.Random().nextInt(360)
+    val deltaX = /*dt * */ entity.velocity * cos(toRadians(angle))
+    val deltaY = /*dt * */ entity.velocity * sin(toRadians(angle))
+    (entity.centerBoundingBox._1 + deltaX, entity.centerBoundingBox._2 + deltaY)
+  }
+
+  private def chaseMovement(entity: Intelligent, chasedEntity: Entity): (Double, Double) = {
+    println("chaseMovement")
+    val angle = toDegrees(atan2(chasedEntity.centerBoundingBox._2 - entity.centerBoundingBox._2, chasedEntity.centerBoundingBox._1 - entity.centerBoundingBox._1))
+    val deltaX = /*dt * */ entity.velocity * cos(toRadians(angle))
+    val deltaY = /*dt * */ entity.velocity * sin(toRadians(angle))
+    (entity.centerBoundingBox._1 + deltaX, entity.centerBoundingBox._2 + deltaY)
+  }
+
+  def crazyMovement(entity: Intelligent, entities: Set[Intelligent]): Intelligent = ???
+
+}
