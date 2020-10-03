196e0655d30fde463df3b95ae7eb02c0e08683a7
unknown <rei.beshiri@studio.unibo.it>
2020-10-03 16:38:53 +0200

    code refactor and rename DegradationEffect

--- src/main/scala/evo_sim/model/DegradationEffect.scala
+++ src/main/scala/evo_sim/model/DegradationEffect.scala
@@ -6,9 +6,11 @@ import evo_sim.model.DomainImpl.Life
  * This object contains the degradation effects that will be passed at the Living entities DegradationEffect property
  * Foods will have simplier degradation effect while Blob will have some specific degradations based on their velocity, dimension, resistance ecc...
  */
-object DegenerationEffect{
+object DegradationEffect{
   //standard degradation effect
-  def standardDegeneration(entity: Entity): Life = 1
-  //blob specific degradation effect
-  def baseDegradation(blob: Blob) : Life = blob.velocity + 1
+  def standardDegradation(entity: Entity): Life = 1
+  //extends the standard Degradation
+  def foodDegradation(entity: Food): Life = standardDegradation(entity)
+  //blob specific degradation effect, velocity + standardDegradation
+  def baseBlobDegradation(blob: Blob) : Life = blob.velocity + standardDegradation(blob)
 }
