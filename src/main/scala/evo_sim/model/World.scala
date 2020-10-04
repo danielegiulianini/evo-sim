15cdd862b5eb40cdc98dccc786f3170da29ad518
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-04 19:39:44 +0200

    fix entities type in World

--- src/main/scala/evo_sim/model/World.scala
+++ src/main/scala/evo_sim/model/World.scala
@@ -2,7 +2,7 @@ package evo_sim.model
 
 import evo_sim.model.EntityBehaviour.Simulable
 
-case class World(currentIteration: Int, entities: Set[_<:Simulable])
+case class World(currentIteration: Int, entities: Set[Simulable])
 
 //companion object
 object World {
