013258d5bec107d7b73944a24d5bd0f4b5f7b8d6
unknown <rei.beshiri@studio.unibo.it>
2020-10-04 22:29:34 +0200

    add SimpleBlob behaviour

--- src/main/scala/evo_sim/model/EntityBehaviour.scala
+++ src/main/scala/evo_sim/model/EntityBehaviour.scala
@@ -1,14 +1,30 @@
 package evo_sim.model
 
 object EntityBehaviour {
-
-  trait Simulable extends Updatable with Collidable
+  trait Simulable extends Updatable with Collidable with Structured //type Simulable = Updatable with Collidable & Structured
 
   //stub for blob
-  class SimulableBlob(baseBlob : BaseBlob) extends Simulable {
-    override def updated(world: World): Set[Simulable] = {print("baseBlob"); Set(this)}
-    override def collided(other: Simulable): Set[Simulable] = Set(this)
+  class SimulableBlob(baseBlob : BaseBlob) extends Simulable {//does nothing
+    override def updated(world: World): Set[Simulable] = {print("baseBlob");Set(baseBlob)}
+    override def collided(other: Simulable): Set[Simulable] = other match {
+      case blob: SimulableBlob => Set(baseBlob)
+      case _ =>Set(baseBlob)
+    }
+    override def structure: Entity = baseBlob
   }
 
   //other entities go here
+/*
+  //stub for food
+  case class SimulableFood(baseFood : BaseFood) extends Simulable {
+    override def updated(world: World): Set[Simulable] = {print("baseFood"); Set(this)}
+    override def collided(other: Entity): Set[Simulable] = {Set(this)}
+  }
+
+  //stub for obstacle
+  case class SimulableObstacle(baseObstacle: BaseObstacle) extends Simulable {
+    override def updated(world: World): Set[Simulable] = {print("baseObstacle"); Set(this)}
+    override def collided(other: Entity): Set[Simulable] = {Set(this)}
+  }
+*/
 }
