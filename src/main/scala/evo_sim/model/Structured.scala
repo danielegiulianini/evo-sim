8c551c3c54b4d31e49cd6239d1dce1879b2f1de5
unknown <rei.beshiri@studio.unibo.it>
2020-10-04 22:28:43 +0200

    add Structured trait

---
+++ src/main/scala/evo_sim/model/Structured.scala
@@ -0,0 +1,14 @@
+package evo_sim.model
+
+import evo_sim.model.EntityBehaviour.Simulable
+
+//DA REFATORIZZARE
+
+trait Structured {
+  def structure: Entity
+}
+
+/**
+ classe provvisoria!
+ serve per tornare la struttura dati dei simulable
+ */
