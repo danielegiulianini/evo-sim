05abae173e37f4f27d70bc181d3abf26c9bf37b5
unknown <rei.beshiri@studio.unibo.it>
2020-10-05 10:27:51 +0200

    refactored Environment as case class

--- src/main/scala/evo_sim/model/Environment.scala
+++ src/main/scala/evo_sim/model/Environment.scala
@@ -1,19 +1,7 @@
 package evo_sim.model
 
-class Environment(temperature: Int,
+case class Environment(temperature: Int,
                   luminosity: Int,
                   initialBlobNumber: Int,
                   initialFoodNumber: Int,
-                  initialObstacleNumber: Int) {
-
-  def Temperature: Int = temperature
-
-  def Luminosity: Int = luminosity
-
-  def InitialBlobNumber: Int = initialBlobNumber
-
-  def InitialFoodNumber: Int = initialFoodNumber
-
-  def InitialObstacleNumber: Int = initialObstacleNumber
-
-}
+                  initialObstacleNumber: Int)
