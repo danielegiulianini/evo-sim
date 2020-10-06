66788f3d0f503e37528d8b3bc539718e445e4eea
alessandro-oliva4 <alessandro.oliva4@studio.unibo.it>
2020-10-06 10:17:23 +0200

    Refactor view hiding impl

--- src/main/scala/evo_sim/controller/Main.scala
+++ src/main/scala/evo_sim/controller/Main.scala
@@ -3,7 +3,7 @@ package evo_sim.controller
 object Main {
 
   def main(args: Array[String]): Unit = {
-    Simulation.apply().launch()
+    Simulation().launch()
   }
 
 }
--- src/main/scala/evo_sim/view/View.scala
+++ src/main/scala/evo_sim/view/View.scala
@@ -10,6 +10,7 @@ import scalafx.scene.paint.Color._
 import scalafx.scene.{Parent, Scene}
 import scalafxml.core.{FXMLView, NoDependencyResolver}
 
+import scala.collection.mutable
 import scala.concurrent.Promise
 
 trait View {
@@ -23,62 +24,65 @@ trait View {
 }
 
 object View {
-  def apply(stage: Stage): View = new FXView(stage)
 
-  private class FXView(stage: Stage) extends View {
+  // "1-bounded" multiton pattern: grants apparent immutability and singleton
+  val instance = mutable.Map.empty[Int, View]
+  def apply(stage: Stage): View = instance.getOrElseUpdate(0, new FXView(stage))
 
-    private var primaryStage: Stage = stage
-    private val inputView: Parent = FXMLView(getClass.getResource("/InputSelector.fxml"),
-      NoDependencyResolver)
-    private val simulatorView: BorderPane = new BorderPane
-    private val entityPane = new BorderPane
-    private val barPane = new BorderPane
+}
 
-    override def inputGUIBuilt(): Unit = {
-      primaryStage = stage
-      primaryStage.title = "evo-sim"
-      barPane.setBorder(new Border(new BorderStroke(Black,
-        BorderStrokeStyle.Solid, CornerRadii.Empty, BorderWidths.Default)))
-      barPane.top = new Label("Info")
-      simulatorView.top = barPane
-      simulatorView.center = entityPane
-    }
+private class FXView(stage: Stage) extends View {
 
-    override def inputReadFromUser(): Promise[Environment] = {
-      primaryStage.scene = new Scene(inputView)
-      primaryStage.resizable = false
-      primaryStage.show()
-      userInput.environment
-    }
+  private var primaryStage: Stage = stage
+  private val inputView: Parent = FXMLView(getClass.getResource("/InputSelector.fxml"),
+    NoDependencyResolver)
+  private val simulatorView: BorderPane = new BorderPane
+  private val entityPane = new BorderPane
+  private val barPane = new BorderPane
 
-    override def simulationGUIBuilt(): Unit = {
-      primaryStage.scene = new Scene(simulatorView, 600, 450)
-    }
+  override def inputGUIBuilt(): Unit = {
+    primaryStage = stage
+    primaryStage.title = "evo-sim"
+    barPane.setBorder(new Border(new BorderStroke(Black,
+      BorderStrokeStyle.Solid, CornerRadii.Empty, BorderWidths.Default)))
+    barPane.top = new Label("Info")
+    simulatorView.top = barPane
+    simulatorView.center = entityPane
+  }
 
-    override def rendered(world: World): Unit = {
-      entityPane.children = world.entities.map(e =>
-        e.structure.boundingBox match {
-          case Circle((x, y), r) => new scalafx.scene.shape.Circle {
-            centerX = x
-            centerY = y
-            radius = r
-            fill = Yellow
-          }
-          case Rectangle((xCord, yCord), w, h) => new scalafx.scene.shape.Rectangle {
-            x = xCord - (w / 2)
-            y = yCord - (h / 2)
-            width = w
-            height = h
-            fill = Red
-          }
-          case Triangle((xCord, yCord), h, a) => new scalafx.scene.shape.Polygon {
-            private val vertices = triangleVertices(Triangle((xCord, yCord), h, a))
-            points.addAll(vertices._1, vertices._2, vertices._3, vertices._4, vertices._5, vertices._6)
-            fill = Green
-          }
-        })
-    }
+  override def inputReadFromUser(): Promise[Environment] = {
+    primaryStage.scene = new Scene(inputView)
+    primaryStage.resizable = false
+    primaryStage.show()
+    userInput.environment
+  }
 
+  override def simulationGUIBuilt(): Unit = {
+    primaryStage.scene = new Scene(simulatorView, 600, 450)
+  }
+
+  override def rendered(world: World): Unit = {
+    entityPane.children = world.entities.map(e =>
+      e.structure.boundingBox match {
+        case Circle((x, y), r) => new scalafx.scene.shape.Circle {
+          centerX = x
+          centerY = y
+          radius = r
+          fill = Yellow
+        }
+        case Rectangle((xCord, yCord), w, h) => new scalafx.scene.shape.Rectangle {
+          x = xCord - (w / 2)
+          y = yCord - (h / 2)
+          width = w
+          height = h
+          fill = Red
+        }
+        case Triangle((xCord, yCord), h, a) => new scalafx.scene.shape.Polygon {
+          private val vertices = triangleVertices(Triangle((xCord, yCord), h, a))
+          points.addAll(vertices._1, vertices._2, vertices._3, vertices._4, vertices._5, vertices._6)
+          fill = Green
+        }
+      })
   }
 
 }
