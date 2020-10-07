5c9fd2c9af609554cea6e3f163d7ca36a62adb80
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-07 18:31:17 +0200

    fix inheritance problem in EntityBehaviour by replacing Simulable type alias with trait

--- src/main/scala/evo_sim/model/EntityBehaviour.scala
+++ src/main/scala/evo_sim/model/EntityBehaviour.scala
@@ -4,12 +4,12 @@ import evo_sim.model.EntityStructure.{Blob, Entity}
 import evo_sim.model.Entities.BaseBlob
 
 object EntityBehaviour {
-  type Simulable = Updatable with Collidable //-able suffix refers to behaviour only
-  type SimulableEntity = Entity with Simulable //trait Simulable
+  trait Simulable extends Updatable with Collidable //-able suffix refers to behaviour only
+  type SimulableEntity = Entity with Simulable
 
   //stub for blob (does nothing)
   trait BlobBehaviour extends Simulable {
-    self: Blob =>
+    self: BaseBlob =>
 
     override def updated(world: World): Set[SimulableEntity] = {
       print("baseBlob");
