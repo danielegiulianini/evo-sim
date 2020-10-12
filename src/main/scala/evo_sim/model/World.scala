b90c95f7556764ca170cb5239616301011029c94
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-12 23:17:36 +0200

    change Set generic type to SimulableEntity in WorldCreated to fix type mismatch error

--- src/main/scala/evo_sim/model/World.scala
+++ src/main/scala/evo_sim/model/World.scala
@@ -8,7 +8,7 @@ case class World(width: Int, height: Int, currentIteration: Int, entities: Set[S
 //companion object
 object World {
   def worldCreated(env: Environment): World = {
-    val blobs: Set[BaseBlob] = Iterator.fill(env.initialBlobNumber)(BaseBlob(
+    val blobs: Set[SimulableEntity] = Iterator.fill(env.initialBlobNumber)(BaseBlob(
       boundingBox = BoundingBox.Rectangle.apply(point = Point2D(20, 15), width = 10, height = 10),
       life = 100,
       velocity = 50,
@@ -16,17 +16,17 @@ object World {
       fieldOfViewRadius = 10,
       movementStrategy = MovingStrategies.baseMovement)).toSet
 
-    val foods: Set[BaseFood] = Iterator.fill(env.initialFoodNumber)(BaseFood(
+    val foods: Set[SimulableEntity] = Iterator.fill(env.initialFoodNumber)(BaseFood(
       boundingBox = BoundingBox.Circle.apply(point = Point2D(100, 100), radius = 10),
       degradationEffect = DegradationEffect.foodDegradation,
       life = 100,
       effect = Effect.standardFoodEffect)).toSet
 
-    val obstacles: Set[BaseObstacle] = Iterator.fill(env.initialObstacleNumber)(BaseObstacle(
+    val obstacles: Set[SimulableEntity] = Iterator.fill(env.initialObstacleNumber)(BaseObstacle(
       boundingBox = BoundingBox.Triangle(point = Point2D(200, 200), height = 15),
       effect = Effect.neutralEffect)).toSet
 
-    val entities: Set[SimulableEntity] = blobs ++ foods ++ obstacles
+    val entities = blobs ++ foods ++ obstacles
 
     World(width = 100, height = 100, currentIteration = 0, entities = entities)
   }
