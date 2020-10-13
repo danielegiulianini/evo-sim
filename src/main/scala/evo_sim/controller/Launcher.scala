031b79740ce31dba6340024bf1d61c4a5f3fa222
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-13 23:29:18 +0200

    add temporary logging utility for easing debugging

--- src/main/scala/evo_sim/controller/Launcher.scala
+++ src/main/scala/evo_sim/controller/Launcher.scala
@@ -1,7 +1,16 @@
 package evo_sim.controller
 
+import cats.effect.{ContextShift, IO}
+import evo_sim.controller.Logging.log
 import evo_sim.core.SimulationEngine
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
 
 object Launcher extends App {
   SimulationEngine.started().unsafeRunSync()
+}
+
+object Logging {
+  def log(message: String) = println(Thread.currentThread.getName+": " + message)
 }
