4009582a6a93fd1ce0f478ed7c395a5070e04930
alessandro-oliva4 <alessandro.oliva4@studio.unibo.it>
2020-10-14 22:20:16 +0200

    refactor from textfields to sliders

--- src/main/scala/evo_sim/view/swing/SwingGUI.scala
+++ src/main/scala/evo_sim/view/swing/SwingGUI.scala
@@ -2,12 +2,10 @@ package evo_sim.view.swing
 
 import java.awt.event.ActionEvent
 import java.awt.{BorderLayout, Dimension, Toolkit}
-import java.text.NumberFormat
 
 import evo_sim.model.{Environment, World}
 import evo_sim.view.GUI
 import javax.swing._
-import javax.swing.text.NumberFormatter
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Promise}
@@ -27,57 +25,50 @@ case class SwingGUI() extends GUI {
       new JLabel("#Blob"),
       new JLabel("#Food"),
       new JLabel("#Obstacle"),
-      new JLabel("Luminosity (L)"),
+      new JLabel("Luminosity (cd)"),
       new JLabel("Temperature (°C)"),
       new JLabel("#Days"))
     val labelPanel = new JPanel
     val labelBoxLayout = new BoxLayout(labelPanel, BoxLayout.Y_AXIS)
     labelPanel.setLayout(labelBoxLayout)
     labels.foreach(l => {
-      l.setBorder(BorderFactory.createEmptyBorder(3, 0, 3, 0))
+      l.setBorder(BorderFactory.createEmptyBorder(18, 0, 18, 0))
       labelPanel.add(l)
     })
     labelPanel.setBorder(BorderFactory.createEmptyBorder(10, 30, 10, 30))
 
-    val format = NumberFormat.getInstance
-    val formatter = new NumberFormatter(format)
-    formatter.setValueClass(classOf[Integer])
-    formatter.setMinimum(0)
-    formatter.setMaximum(Integer.MAX_VALUE)
-    formatter.setAllowsInvalid(false)
-    formatter.setCommitsOnValidEdit(true)
-
-    val blobTextField = new JFormattedTextField(formatter)
-    val foodTextField = new JFormattedTextField(formatter)
-    val obstacleTextField = new JFormattedTextField(formatter)
-    val luminosityTextField = new JFormattedTextField(formatter)
-    val temperatureField = new JFormattedTextField(formatter)
-    val daysField = new JFormattedTextField(formatter)
-    blobTextField.setColumns(10)
-    foodTextField.setColumns(10)
-    obstacleTextField.setColumns(10)
-    luminosityTextField.setColumns(10)
-    temperatureField.setColumns(10)
-    daysField.setColumns(10)
+    val blobSlider = new JSlider(0, 300, 50)
+    val foodSlider = new JSlider(0, 300, 50)
+    val obstacleSlider = new JSlider(0, 100, 20)
+    val luminositySlider = new JSlider(10, 200, 100)
+    val temperatureSlider = new JSlider(-10, 50, 20)
+    val daysSlider = new JSlider(1, 366, 30)
+    Set(blobSlider, foodSlider, obstacleSlider, luminositySlider, temperatureSlider, daysSlider).foreach(s => {
+      s.setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 0))
+      s.setMajorTickSpacing(s.getMaximum / 5)
+      s.setMinorTickSpacing(1)
+      s.setPaintTicks(true)
+      s.setPaintLabels(true)
+    })
     val textFieldPanel = new JPanel
     val textFieldBoxLayout = new BoxLayout(textFieldPanel, BoxLayout.Y_AXIS)
     textFieldPanel.setLayout(textFieldBoxLayout)
-    textFieldPanel.add(blobTextField)
-    textFieldPanel.add(foodTextField)
-    textFieldPanel.add(obstacleTextField)
-    textFieldPanel.add(luminosityTextField)
-    textFieldPanel.add(temperatureField)
-    textFieldPanel.add(daysField)
+    textFieldPanel.add(blobSlider)
+    textFieldPanel.add(foodSlider)
+    textFieldPanel.add(obstacleSlider)
+    textFieldPanel.add(luminositySlider)
+    textFieldPanel.add(temperatureSlider)
+    textFieldPanel.add(daysSlider)
     textFieldPanel.setBorder(BorderFactory.createEmptyBorder(10, 30, 10, 30))
 
     val startButton = new JButton("Start")
     startButton.addActionListener((_: ActionEvent) => userInput.success(Environment(
-      temperature = blobTextField.getText().toInt,
-      luminosity = luminosityTextField.getText().toInt,
-      initialBlobNumber = blobTextField.getText().toInt,
-      initialFoodNumber = foodTextField.getText().toInt,
-      initialObstacleNumber = obstacleTextField.getText().toInt,
-      daysNumber = daysField.getText().toInt
+      temperature = temperatureSlider.getValue,
+      luminosity = luminositySlider.getValue,
+      initialBlobNumber = blobSlider.getValue,
+      initialFoodNumber = foodSlider.getValue,
+      initialObstacleNumber = obstacleSlider.getValue,
+      daysNumber = daysSlider.getValue
     )))
 
     SwingUtilities.invokeAndWait(() => {
@@ -86,7 +77,6 @@ case class SwingGUI() extends GUI {
       frame.getContentPane.add(textFieldPanel, BorderLayout.EAST)
       frame.getContentPane.add(startButton, BorderLayout.SOUTH)
       frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
-      frame.setSize(800, 800)
       frame.pack()
     })
   }
