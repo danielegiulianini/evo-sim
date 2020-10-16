953c0a3a0d376d808494516eca98a7be8124da44
Andrea Vaienti <andrea.vaienti2@studio.unibo.it>
2020-10-17 01:50:45 +0200

    MovingStrategies FP version

--- src/main/scala/evo_sim/model/MovingStrategies.scala
+++ src/main/scala/evo_sim/model/MovingStrategies.scala
@@ -1,7 +1,7 @@
 package evo_sim.model
 
 import evo_sim.model.EntityBehaviour.SimulableEntity
-import evo_sim.model.EntityStructure.Intelligent
+import evo_sim.model.EntityStructure.{Food, Intelligent}
 
 import scala.math._
 
@@ -13,23 +13,25 @@ object MovingStrategies {
 
   def baseMovement(entity: Intelligent, world: World): Movement = {
 
-    val chasedEntity = world.entities - entity.asInstanceOf[SimulableEntity] match {
+    val chasedEntity = (world.entities - entity.asInstanceOf[SimulableEntity]).filter(elem => elem.isInstanceOf[Food]) match {
       case set if set.nonEmpty => Option(set.minBy(distanceBetweenEntities(entity, _)))
       case _ => None
     }
 
     chasedEntity match {
-      //case Some(chasedEntity) if distanceBetweenEntities(entity, chasedEntity) < entity.fieldOfViewRadius => chaseMovement(entity, chasedEntity)
+      case Some(chasedEntity) if (distanceBetweenEntities(entity, chasedEntity) < entity.fieldOfViewRadius) => {
+        /*println("entity: " + entity.boundingBox)
+        println("chased: " + chasedEntity.boundingBox)
+        println("FOV: " + entity.fieldOfViewRadius + ", distance: " + distanceBetweenEntities(entity, chasedEntity))
+        Thread.sleep(10000)*/
+        chaseMovement(entity, chasedEntity)
+      }
       case _ => standardMovement(entity, entity.direction.angle, world)
     }
 
   }
 
   //def crazyMovement(entity: Intelligent, entities: Set[Intelligent]): Intelligent = ???
-
-  private def distanceBetweenEntities(a: Intelligent, b: SimulableEntity): Double = {
-    sqrt(pow(b.boundingBox.point.x - a.boundingBox.point.x, 2) + pow(b.boundingBox.point.y - a.boundingBox.point.y, 2))
-  }
 
   @scala.annotation.tailrec
   private def standardMovement(entity: Intelligent, angle: Int, world: World): Movement = {
@@ -38,31 +40,35 @@ object MovingStrategies {
       case x => Direction(angle, x-1)
     }
 
-    val deltaX = /*dt * */ entity.velocity * cos(toRadians(direction.angle)) * 0.05
-    val deltaY = /*dt * */ entity.velocity * sin(toRadians(direction.angle)) * 0.05
-    val x = (entity.boundingBox.point.x + deltaX).toFloat.round
-    val y = (entity.boundingBox.point.y + deltaY).toFloat.round
+    val positionUpdated = nextPosition(entity, direction.angle)
 
-    isBoundaryCollision(Point2D(x, y), Point2D(world.width, world.height)) match {
+    isBoundaryCollision(positionUpdated, Point2D(world.width, world.height)) match {
       case true => standardMovement(entity, random.nextInt(360), world)
-      case false => Movement(Point2D(x, y), direction)
+      case false => Movement(positionUpdated, direction)
     }
   }
 
-  /*@scala.annotation.tailrec
-  private def chaseMovement(entity: Intelligent, chasedEntity: SimulableEntity): Point2D = {
-    val angle = toDegrees(atan2(chasedEntity.boundingBox.point.y - entity.boundingBox.point.y, chasedEntity.boundingBox.point.x - entity.boundingBox.point.x))
-    val deltaX = /*dt * */ entity.velocity * cos(toRadians(angle))
-    val deltaY = /*dt * */ entity.velocity * sin(toRadians(angle))
-    val x = entity.boundingBox.point.x + deltaX
-    val y = entity.boundingBox.point.y + deltaY
-    if (isBoundaryCollision(x, y)) chaseMovement(entity, chasedEntity) else Point2D(x.toInt, y.toInt)
-  }*/
+  private def nextPosition(entity: Intelligent, angle: Int): Point2D = {
+    val deltaX = /*dt * */ entity.velocity * cos(toRadians(angle)) * 0.05
+    val deltaY = /*dt * */ entity.velocity * sin(toRadians(angle)) * 0.05
+    val x = (entity.boundingBox.point.x + deltaX).toFloat.round
+    val y = (entity.boundingBox.point.y + deltaY).toFloat.round
+    Point2D(x, y)
+  }
+
+  private def chaseMovement(entity: Intelligent, chasedEntity: SimulableEntity): Movement = {
+    val angle = toDegrees(atan2(chasedEntity.boundingBox.point.y - entity.boundingBox.point.y, chasedEntity.boundingBox.point.x - entity.boundingBox.point.x)).toFloat.round
+    Movement(nextPosition(entity, angle), Direction(angle, entity.direction.stepToNextDirection - 1))
+  }
 
   //TODO: Bisogna considerare anche il raggio di grandezza del blob
   private def isBoundaryCollision(entityPosition: Point2D, worldDimension: Point2D): Boolean = (entityPosition.x, entityPosition.y) match {
     case (x, y) if (0 until worldDimension.x contains x) && (0 until worldDimension.y contains y) => false
     case _ => true
+  }
+
+  private def distanceBetweenEntities(a: Intelligent, b: SimulableEntity): Double = {
+    sqrt(pow(b.boundingBox.point.x - a.boundingBox.point.x, 2) + pow(b.boundingBox.point.y - a.boundingBox.point.y, 2))
   }
 
 
