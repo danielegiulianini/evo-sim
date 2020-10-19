52c0ab2f3044345ca767e942a7d4bdb104a024da
unknown <rei.beshiri@studio.unibo.it>
2020-10-19 17:29:41 +0200

    add comment in luminosity and temperature effect

--- src/main/scala/evo_sim/model/LuminosityEffect.scala
+++ src/main/scala/evo_sim/model/LuminosityEffect.scala
@@ -9,18 +9,8 @@ object LuminosityEffect {
     val deviation: Int = 3 //CONST -> si può calibrare
     val x: Int = (deviation * Math.sin(2.00 * (currentIteration / Constants.ITERATIONS_PER_DAY.toDouble * Math.PI))).toInt
     val lambda: Int = 2   //normalizzatore (volendo si possono usare altre tecniche di normalizzazione)
-    val normalize: Int = 2
+    val normalize: Int = 2 //-> valore della temperatura deciso dall'utente
     ((x / lambda) * deviation / normalize)
   }
-
-
-  /**
-   * def f(i: Int): Integer =  {
-   * val deviation: Int = 2 //CONST -> si può calibrare
-   * val x: Int = (deviation * Math.sin(2.00 * (i / Constants.ITERATIONS_PER_DAY * Math.PI))).toInt
-   * val lambda: Int = 1   //normalizzatore (volendo si possono usare altre tecniche di normalizzazione)
-   * return (x / lambda) * deviation
-   * }
-   */
 
 }
--- src/main/scala/evo_sim/model/TemperatureEffect.scala
+++ src/main/scala/evo_sim/model/TemperatureEffect.scala
@@ -10,7 +10,7 @@ object TemperatureEffect {
     val deviation: Int = 3 //CONST -> si può calibrare
     val x: Int = (deviation * Math.sin(2.00 * (currentIteration / Constants.ITERATIONS_PER_DAY.toDouble * Math.PI))).toInt
     val lambda: Int = 2   //normalizzatore (volendo si possono usare altre tecniche di normalizzazione)
-    val normalize: Int = 3
+    val normalize: Int = 3 //-> valore della temperatura deciso dall'utente
     ((x / lambda) * deviation / normalize)
   }
 }
