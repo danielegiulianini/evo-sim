491a2e3d90e834a97fb093472487b85e8811495b
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-19 23:59:47 +0200

    add stub for test 1 of World

--- src/test/scala/TestSimulationLoop.scala
+++ src/test/scala/TestSimulationLoop.scala
@@ -10,12 +10,12 @@ class TestSimulationLoop {
     initialBlobNumber= 10,
     initialFoodNumber= 3,
     initialObstacleNumber= 3,
-    daysNumber=5)
+    daysNumber = 5)
 
   //test 1
   val w = simulationLoop().runS(World(initialEnvironment)).run()
   w.currentIteration //should be equal to daysNumber * iterationPerDay
 
   //test 2
-  
+
 }
---
+++ src/test/scala/TestWorld.scala
@@ -0,0 +1,6 @@
+class TestWorld {
+
+  //test 1: for valid parameters passing after initialization
+
+  //test 2:
+}
