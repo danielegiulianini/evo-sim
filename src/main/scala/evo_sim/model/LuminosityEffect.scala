4752050ddd23221230d3ad32a05c1bbe02c23377
unknown <rei.beshiri@studio.unibo.it>
2020-10-19 16:59:52 +0200

    nomalize luminosityEffect return value

--- src/main/scala/evo_sim/model/LuminosityEffect.scala
+++ src/main/scala/evo_sim/model/LuminosityEffect.scala
@@ -6,10 +6,11 @@ object LuminosityEffect {
 
   // TODO: normalizzare sul range della luminosità raggiungibile (?)
   def standardLuminosityEffect(currentIteration: Int): Int ={
-    val deviation: Int = 2 //CONST -> si può calibrare
+    val deviation: Int = 3 //CONST -> si può calibrare
     val x: Int = (deviation * Math.sin(2.00 * (currentIteration / Constants.ITERATIONS_PER_DAY.toDouble * Math.PI))).toInt
-    val lambda: Int = 1   //normalizzatore (volendo si possono usare altre tecniche di normalizzazione)
-    (x / lambda) * deviation
+    val lambda: Int = 2   //normalizzatore (volendo si possono usare altre tecniche di normalizzazione)
+    val normalize: Int = 2
+    ((x / lambda) * deviation / normalize)
   }
 
 
