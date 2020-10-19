669922cf4316864acbd79b3b66193e71db688a71
unknown <rei.beshiri@studio.unibo.it>
2020-10-19 16:28:41 +0200

    add sin function prototype

--- src/main/scala/evo_sim/model/LuminosityEffect.scala
+++ src/main/scala/evo_sim/model/LuminosityEffect.scala
@@ -8,4 +8,13 @@ object LuminosityEffect {
   def standardLuminosityEffect(blob: Blob, luminosity: Int): Int =
     blob.fieldOfViewRadius + (1 * Math.sin(2 * Math.PI * luminosity / Constants.ITERATIONS_PER_DAY)).toInt
 
+  /**
+   * def f(i: Int): Integer =  {
+   * val deviation: Int = 2 //CONST -> si può calibrare
+   * val x: Int = (deviation * Math.sin(2.00 * (i / Constants.ITERATIONS_PER_DAY * Math.PI))).toInt
+   * val lambda: Int = 1   //normalizzatore (volendo si possono usare altre tecniche di normalizzazione)
+   * return (x / lambda) * deviation
+   * }
+   */
+
 }
