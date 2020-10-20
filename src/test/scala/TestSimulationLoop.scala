d93a92441ad20b83847d880110154f98671e3849
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-20 16:43:19 +0200

    correct indentation of test file

--- src/test/scala/TestSimulationLoop.scala
+++ src/test/scala/TestSimulationLoop.scala
@@ -5,11 +5,12 @@ import evo_sim.core.Simulation._
 
 class TestSimulationLoop {
 
-  val initialEnvironment : Environment = Environment(temperature = 2,
+  val initialEnvironment : Environment = Environment(
+    temperature = 2,
     luminosity = 3,
-    initialBlobNumber= 10,
-    initialFoodNumber= 3,
-    initialObstacleNumber= 3,
+    initialBlobNumber = 10,
+    initialFoodNumber = 3,
+    initialObstacleNumber = 3,
     daysNumber = 5)
 
   val initialWorld = World(initialEnvironment)
@@ -18,7 +19,8 @@ class TestSimulationLoop {
   val w = simulationLoop().runS(initialWorld).run()
   w.currentIteration //should be equal to daysNumber * iterationPerDay
 
-  //test 2: check for constant framerate of gameLoop
-  
+  //test 2: check for constant frame rate of simulationLoop
+
+
 
 }
