2bbb0259c9d5ab5312c69d9b01cd45eec7c9ff8d
unknown <rei.beshiri@studio.unibo.it>
2020-10-20 19:54:24 +0200

    fix simulation blob creation and luminosity

--- src/main/scala/evo_sim/model/Effect.scala
+++ src/main/scala/evo_sim/model/Effect.scala
@@ -1,5 +1,6 @@
 package evo_sim.model
 
+import evo_sim.model.BoundingBox.Circle
 import evo_sim.model.Entities.{BaseBlob, PoisonBlob, SlowBlob}
 import evo_sim.model.EntityBehaviour.SimulableEntity
 import evo_sim.model.EntityStructure.Blob
@@ -13,18 +14,20 @@ object Effect {
       blob.velocity,
       blob.degradationEffect,
       blob.fieldOfViewRadius,
-      MovingStrategies.baseMovement, blob.direction/*blob.movementDirection, blob.stepToNextDirection*/)
+      MovingStrategies.baseMovement, blob.direction)
     Set(updatedBlob)
   }
 
   def reproduceBlobFoodEffect(blob: Blob): Set[SimulableEntity] = {
-    val newBlob = BaseBlob(blob.name+"-son", blob.boundingBox, blob.life + Constants.DEF_FOOD_ENERGY,
+    val newBlob = BaseBlob(blob.name+"-son",
+      blob.boundingBox,
+      Constants.DEF_BLOB_LIFE,
       randomValueChange(blob.velocity, Constants.DEF_MOD_PROP_RANGE), blob.degradationEffect,
       randomValueChange(blob.fieldOfViewRadius, Constants.DEF_MOD_PROP_RANGE),
-      MovingStrategies.baseMovement, blob.direction/*blob.movementDirection, blob.stepToNextDirection*/)
+      MovingStrategies.baseMovement, Direction(blob.direction.angle, Constants.NEXT_DIRECTION))
     Set(newBlob, blob match {
       case b : BaseBlob => BaseBlob(b.name, b.boundingBox, b.life + Constants.DEF_FOOD_ENERGY,
-        b.velocity, b.degradationEffect, b.fieldOfViewRadius, b.movementStrategy, b.direction/*b.movementDirection, b.stepToNextDirection*/)
+        b.velocity, b.degradationEffect, b.fieldOfViewRadius, b.movementStrategy, b.direction)
       case b: PoisonBlob => b.copy()
       case b: SlowBlob => b.copy()
     })
@@ -36,11 +39,11 @@ object Effect {
   }
 
   // used for static entities
-  def neutralEffect(blob: Blob): Set[SimulableEntity] = {
-    blob match {
-      case b : Blob => Set(b)
-      case _ => Set()
-    }
+  def neutralEffect(blob: Blob): Set[SimulableEntity] = blob match {
+    case base: BaseBlob => Set(base.copy())
+    case poison: PoisonBlob => Set(poison.copy())
+    case slow: SlowBlob => Set(slow.copy())
+    case _ => Set()
   }
 
   def mudEffect(blob: Blob): Set[SimulableEntity] = {
@@ -53,8 +56,7 @@ object Effect {
   }
 
   def damageEffect(blob: Blob): Set[SimulableEntity] = blob match {
-    case b: BaseBlob => Set(BaseBlob(b.name, b.boundingBox, b.life - Constants.DEF_DAMAGE, b.velocity,
-      b.degradationEffect, b.fieldOfViewRadius, b.movementStrategy, b.direction))
+    case base: BaseBlob => Set(base.copy(life = base.life - Constants.DEF_DAMAGE))
     case _ => Set()
     }
 
