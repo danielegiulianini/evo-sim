a9e172f192efadb5b462c5e4b9bbf032e4b63415
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-22 10:46:50 +0200

    add test for checking if new sinusoidal version works like the previous

--- src/main/scala/evo_sim/model/World.scala
+++ src/main/scala/evo_sim/model/World.scala
@@ -18,47 +18,47 @@ case class World(temperature: Int,
 //companion object
 object World {
 
-  def randomPosition(): Point2D = Point2D(new scala.util.Random().nextInt(Constants.WORLD_WIDTH + 1),
-    new scala.util.Random().nextInt(Constants.WORLD_HEIGHT + 1))
+  def randomPosition(): Point2D = Point2D.apply(new scala.util.Random().nextInt(Constants.WORLD_WIDTH.+(1)),
+    new scala.util.Random().nextInt(Constants.WORLD_HEIGHT.+(1)))
 
   def apply(env: Environment): World = {
-    val blobs: Set[BaseBlob] = Iterator.tabulate(env.initialBlobNumber)(i => BaseBlob(
-      name = "blob" + i,
-      boundingBox = BoundingBox.Circle(point = randomPosition(), radius = Constants.DEF_BLOB_RADIUS),
+    val blobs: Set[BaseBlob] = Iterator.tabulate(env.initialBlobNumber)((i: Int) => BaseBlob.apply(
+      name = "blob".+(i),
+      boundingBox = BoundingBox.Circle(point = World.randomPosition(), radius = Constants.DEF_BLOB_RADIUS),
       life = Constants.DEF_BLOB_LIFE,
       velocity = Constants.DEF_BLOB_VELOCITY,
-      degradationEffect = DegradationEffect.standardDegradation,
+      degradationEffect = (blob: EntityStructure.Blob) => DegradationEffect.standardDegradation(blob),
       fieldOfViewRadius = Constants.DEF_BLOB_FOW_RADIUS,
       movementStrategy = MovingStrategies.baseMovement,
-      direction = Direction(0, 20))).toSet
+      direction = Direction.apply(0, 20))).toSet
 
-    val standardFoods: Set[BaseFood] = Iterator.tabulate((env.initialFoodNumber / 2).ceil.toInt)(i => BaseFood(
-      name = "standardFood" + i,
-      boundingBox = BoundingBox.Triangle(point = randomPosition(), height = Constants.DEF_FOOD_HEIGHT),
-      degradationEffect = DegradationEffect.foodDegradation,
+    val standardFoods: Set[BaseFood] = Iterator.tabulate((env.initialFoodNumber./(2)).ceil.toInt)((i: Int) => BaseFood.apply(
+      name = "standardFood".+(i),
+      boundingBox = BoundingBox.Triangle(point = World.randomPosition(), height = Constants.DEF_FOOD_HEIGHT),
+      degradationEffect = (food: EntityStructure.Food) => DegradationEffect.foodDegradation(food),
       life = Constants.DEF_FOOD_LIFE,
       effect = Effect.standardFoodEffect)).toSet
 
-    val reproducingFoods: Set[BaseFood] = Iterator.tabulate((env.initialFoodNumber / 2).floor.toInt)(i => BaseFood(
-      name = "reproducingFood" + i,
-      boundingBox = BoundingBox.Triangle(point = randomPosition(), height = Constants.DEF_REPRODUCING_FOOD_HEIGHT),
-      degradationEffect = DegradationEffect.foodDegradation,
+    val reproducingFoods: Set[BaseFood] = Iterator.tabulate((env.initialFoodNumber./(2)).floor.toInt)((i: Int) => BaseFood.apply(
+      name = "reproducingFood".+(i),
+      boundingBox = BoundingBox.Triangle(point = World.randomPosition(), height = Constants.DEF_REPRODUCING_FOOD_HEIGHT),
+      degradationEffect = (food: EntityStructure.Food) => DegradationEffect.foodDegradation(food),
       life = Constants.DEF_FOOD_LIFE,
       effect = Effect.reproduceBlobFoodEffect)).toSet
 
-    val stones: Set[BaseObstacle] = Iterator.tabulate((env.initialObstacleNumber / 2).ceil.toInt)(i => BaseObstacle(
-      name = "stone" + i,
-      boundingBox = BoundingBox.Rectangle(point = randomPosition(), width = Constants.DEF_STONE_WIDTH, height = Constants.DEF_STONE_HEIGHT),
+    val stones: Set[BaseObstacle] = Iterator.tabulate((env.initialObstacleNumber./(2)).ceil.toInt)((i: Int) => BaseObstacle.apply(
+      name = "stone".+(i),
+      boundingBox = BoundingBox.Rectangle(point = World.randomPosition(), width = Constants.DEF_STONE_WIDTH, height = Constants.DEF_STONE_HEIGHT),
       effect = Effect.damageEffect)).toSet
 
-    val puddles: Set[BaseObstacle] = Iterator.tabulate((env.initialObstacleNumber / 2).floor.toInt)(i => BaseObstacle(
-      name = "puddle" + i,
-      boundingBox = BoundingBox.Rectangle(point = randomPosition(), width = Constants.DEF_PUDDLE_WIDTH, height = Constants.DEF_PUDDLE_HEIGHT),
+    val puddles: Set[BaseObstacle] = Iterator.tabulate((env.initialObstacleNumber./(2)).floor.toInt)((i: Int) => BaseObstacle.apply(
+      name = "puddle".+(i),
+      boundingBox = BoundingBox.Rectangle(point = World.randomPosition(), width = Constants.DEF_PUDDLE_WIDTH, height = Constants.DEF_PUDDLE_HEIGHT),
       effect = Effect.mudEffect)).toSet
 
-    val entities: Set[SimulableEntity] = blobs ++ standardFoods ++ reproducingFoods ++ stones ++ puddles
+    val entities: Set[SimulableEntity] = (blobs ++ standardFoods ++ reproducingFoods ++ stones.++(puddles)) (Set.canBuildFrom)
 
-    World(temperature = env.temperature, luminosity = env.luminosity, width = Constants.WORLD_WIDTH, height = Constants.WORLD_HEIGHT,
+    World.apply(temperature = env.temperature, luminosity = env.luminosity, width = Constants.WORLD_WIDTH, height = Constants.WORLD_HEIGHT,
       currentIteration = 0, entities = entities, totalIterations = env.daysNumber * Constants.ITERATIONS_PER_DAY)
   }
 
@@ -66,51 +66,54 @@ object World {
 
   def worldEnvironmentUpdated(world: World): EnvironmentParameters = {
 
-    val luminosityUpdated: ((Int, Int)) => Int = MemoHelper.memoize {
+    val luminosityUpdated: Function1[Tuple2[Int, Int], Int] = MemoHelper.memoize({
       case (luminosity, currentIteration) =>
-        luminosity + zeroPhasedSinusoidalSin(1 + 1/32f, currentIteration / Constants.ITERATIONS_PER_DAY, 0)
-    }
+        luminosity + TrigonometricalOps.zeroPhasedSinusoidalSin(1 + 1/32f, currentIteration / Constants.ITERATIONS_PER_DAY, 0)
+    })
 
     /*def updatedLuminosity(luminosity: Int, currentIteration: Int) =
   luminosity + ((1 + 1 / 32f) * Math.sin(2 * Math.PI * currentIteration / Constants.ITERATIONS_PER_DAY)).toInt*/
 
 
-    val temperatureUpdated: ((Int, Int)) => Int = MemoHelper.memoize {
+    val temperatureUpdated: Function1[Tuple2[Int, Int], Int] = MemoHelper.memoize({
       case (temperature, currentIteration) =>
-        temperature + zeroPhasedOneYTranslatedSinusoidalSin(1 + 1/64f, currentIteration/Constants.ITERATIONS_PER_DAY)
-    }
+        temperature + TrigonometricalOps.zeroPhasedOneYTranslatedSinusoidalSin(1 + 1/64f, currentIteration/Constants.ITERATIONS_PER_DAY)
+    })
 
     /*def updatedTemperature(tempera: Int, currentIteration: Int) =
   luminosity + ((1 + 1 / 64f) * Math.sin(2 * Math.PI * currentIteration / Constants.ITERATIONS_PER_DAY)).toInt*/
 
 
-    EnvironmentParameters(luminosityUpdated(world.luminosity, world.currentIteration),
+    EnvironmentParameters.apply(luminosityUpdated(world.luminosity, world.currentIteration),
       temperatureUpdated(world.temperature, world.currentIteration))
   }
 
 
 
   object MemoHelper {
-    def memoize[I, O](f: I => O): I => O = new collection.mutable.HashMap[I, O]() {
-      override def apply(key: I) = getOrElseUpdate(key, f(key))
+    def memoize[I, O](f: Function1[I, O]): Function1[I, O] = new collection.mutable.HashMap[I, O]() {
+      override def apply(key: I): O = getOrElseUpdate(key, f(key))
     }
   }
 
   object TrigonometricalOps {
-    def sinusoidalSin(yDilatation: Float)(x:Float)(phase: Int)(yTranslation: Int) =
+    def sinusoidalSin(yDilatation: Float)(x:Float)(phase: Int)(yTranslation: Int): Int =
       (yDilatation * Math.sin(2 * Math.PI * x + phase)).toInt + yTranslation  //should rename ytranslation to amplitude
 
     //most used, common and popular sinusoidalSin invocations (for this purpose translated in partially-applied functions)
-    def zeroPhasedSinusoidalSin = sinusoidalSin  (_:Float) (_:Float) (0) (_:Int)
-    def zeroYTranslatedSinusoidalSin = sinusoidalSin (_:Float) (_:Float) (_:Int) (0)
-    def oneYTranslatedSinusoidalSin = sinusoidalSin (_:Float) (_:Float) (_:Int) (1)
-    def zeroPhasedOneYTranslatedSinusoidalSin : (Float, Float) => Int = sinusoidalSin (_:Float) (_:Float) (0)  (1)
+    def zeroPhasedSinusoidalSin: Function3[Float, Float, Int, Int] = (fl1: Float, fl2: Float, i: Int) => TrigonometricalOps.sinusoidalSin  (fl1) (fl2) (0) (i)
+
+    def zeroYTranslatedSinusoidalSin: Function3[Float, Float, Int, Int] = (fl1: Float, fl2: Float, i: Int) => TrigonometricalOps.sinusoidalSin (fl1) (fl2) (i) (0)
+
+    def oneYTranslatedSinusoidalSin: Function3[Float, Float, Int, Int] = (fl1: Float, fl2: Float, i: Int) => TrigonometricalOps.sinusoidalSin (fl1) (fl2) (i) (1)
+
+    def zeroPhasedOneYTranslatedSinusoidalSin : Function2[Float, Float, Int] = (fl1: Float, fl2: Float) => TrigonometricalOps.sinusoidalSin (fl1) (fl2) (0)  (1)
 
     object Curried {
-      def zeroPhasedSinusoidalSin = TrigonometricalOps.zeroPhasedSinusoidalSin.curried
-      def zeroYTranslatedSinusoidalSin = TrigonometricalOps.zeroYTranslatedSinusoidalSin.curried
-      def oneYTranslatedSinusoidalSin = TrigonometricalOps.oneYTranslatedSinusoidalSin.curried
-      def zeroPhasedOneYTranslatedSinusoidalSin =TrigonometricalOps.zeroPhasedOneYTranslatedSinusoidalSin.curried
+      def zeroPhasedSinusoidalSin: Function1[Float, Float => Int => Int] = TrigonometricalOps.zeroPhasedSinusoidalSin.curried
+      def zeroYTranslatedSinusoidalSin: Function1[Float, Float => Int => Int] = TrigonometricalOps.zeroYTranslatedSinusoidalSin.curried
+      def oneYTranslatedSinusoidalSin: Function1[Float, Float => Int => Int] = TrigonometricalOps.oneYTranslatedSinusoidalSin.curried
+      def zeroPhasedOneYTranslatedSinusoidalSin: Function1[Float, Float => Int] =TrigonometricalOps.zeroPhasedOneYTranslatedSinusoidalSin.curried
     }
 
     /*example of use:
--- src/test/scala/TestTrigonometricalOps.scala
+++ src/test/scala/TestTrigonometricalOps.scala
@@ -1,21 +1,43 @@
+import evo_sim.model.Constants
 import evo_sim.model.World.TrigonometricalOps.Curried._
 import evo_sim.model.World.TrigonometricalOps.sinusoidalSin
 import org.scalacheck.{Arbitrary, Prop, Properties}
 import org.scalacheck.Prop.{exists, forAll}
 
+
+//run with: test;  from Intellij sbt shell
+//or: sbt test     from cmd from the the folder cotaining build.sbt file
 object SinusoidalSpecifications extends Properties("Sinusoidal") {
   //signature: sinusoidalSin(yDilatation: Float)(x:Float)(phase: Int)(yTranslation: Int)
 
-  //1. check codomain is respected for a subset of the values (max = amplitude * 1 + ytranslation, min = amplitude * -1 + ytraslation)
-  property("max and min value respected") = forAll {
+  //1. check codomain is respected (max = amplitude * 1 + ytranslation, min = amplitude * -1 + ytraslation)
+  /*property("max and min value respected") = forAll {
     (yDilatation: Float, x: Float, phase: Int, yTranslation: Int) => {
       val value = sinusoidalSin(yDilatation)(x)(phase)(yTranslation: Int)
       value <= yDilatation /* *1 */ + yTranslation || value >= -yDilatation /* *1 */ + yTranslation
     }
-  }
+  }*/
 
   //2. check periodic cycle
 
+  //3. check reversing phase sign results in reversed wave
+
+  //4. check amplitude change results in proportional result change
+
+
+  //x. check it behaves like the previous working version
+  val oldYDilatation = 1 + 1 / 32f
+
+  def oldSinusoidal(x: Int) =
+    ((oldYDilatation) * Math.sin(2 * Math.PI * x / Constants.ITERATIONS_PER_DAY)).toInt
+
+  property("new sinusoidal result equals old (working but not tested) sinusoidal result") = forAll {
+    (x: Int) => {
+      val newVersionValue = sinusoidalSin((oldYDilatation))(x / Constants.ITERATIONS_PER_DAY)(0)(0)
+      val oldVersionValue = oldSinusoidal(x)
+      newVersionValue == oldVersionValue
+    }
+  }
 }
 
 
