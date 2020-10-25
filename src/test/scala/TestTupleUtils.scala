c123f438ea346e737d7630780e11331b81d923fd
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-25 21:26:35 +0100

    add testset2 for TupleUtils test

--- src/test/scala/TestTupleUtils.scala
+++ src/test/scala/TestTupleUtils.scala
@@ -9,4 +9,6 @@ class TestTupleUtils {
   println("the set " + testSet +" contains any of: " + tupleSet + "?:" + containedAnyOf(testSet, tupleSet))
   println("resulting is :" + everyElementPairedWithOnlyOneOtherElement(testSet))
 
+  val testSet2 = Set((1, 2), (1, 3), (3, 1))
+  println("il set filtrato e': " + TupleUtils.everyElementPairedWithOnlyOneOtherElement(testSet2))
 }
