372c58fde968dc4737dcfcb620d7d631015a5917
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-26 00:09:01 +0100

    refactor JFrameIO packed

--- src/main/scala/evo_sim/view/swing/monadic/JFrameIO.scala
+++ src/main/scala/evo_sim/view/swing/monadic/JFrameIO.scala
@@ -1,14 +1,10 @@
 package evo_sim.view.swing.monadic
 
-import java.awt.{Dimension, Toolkit}
-
 import cats.effect.IO
 import javax.swing.{JFrame, SwingUtilities}
 
 class JFrameIO(jFrame: JFrame) extends JComponentIO {
   def resizedInvokingAndWaiting(frame: JFrame): IO[Unit] = IO { SwingUtilities.invokeAndWait(() => frame.setResizable(false))}
   def visibleSetInvokingAndWaiting(b: Boolean): IO[Unit] = IO{ SwingUtilities.invokeAndWait(() => jFrame.setVisible(b)) }
-  def packedInvokingAndWaiting(): IO[Unit] = IO apply SwingUtilities.invokeAndWait(() => jFrame.pack())
-
-
+  def packedInvokingAndWaiting(): IO[Unit] = IO { SwingUtilities.invokeAndWait(() => jFrame.pack()) }
 }
