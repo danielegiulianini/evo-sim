68e4baf4de4d8d7e0d534a1e0e4cb6a9eff2e540
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-26 22:13:15 +0100

    make ContainerIO generic

--- src/main/scala/evo_sim/view/swing/monadic/ContainerIO.scala
+++ src/main/scala/evo_sim/view/swing/monadic/ContainerIO.scala
@@ -4,14 +4,14 @@ import java.awt.{Component, Container, LayoutManager}
 
 import cats.effect.IO
 
-class ContainerIO(container: Container) extends ComponentIO(container) {
-  def added(component: ComponentIO) = IO {
+class ContainerIO[T<:Container](container: Container) extends ComponentIO(container) {
+  def added(component: ComponentIO[ _<:Component]) = IO {
     container.add(component.component)
   }
-  def added(name: String, component: ComponentIO) = IO {
+  def added(name: String, component: ComponentIO[ _<:Component]) = IO {
     container.add(name, component.component)
   }
-  def added(component: ComponentIO, constraints : Object) = IO {
+  def added(component: ComponentIO[ _<:Component], constraints : Object) = IO {
     container.add(component.component, constraints)
   }
   def layoutSet(mgr : LayoutManager): IO[Unit] = IO {
