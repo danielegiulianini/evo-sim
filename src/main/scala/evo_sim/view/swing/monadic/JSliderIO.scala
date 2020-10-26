a6bc3db7e45222415ea1f15a64e302b05944e661
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-26 15:21:29 +0100

    add JSliderIO

---
+++ src/main/scala/evo_sim/view/swing/monadic/JSliderIO.scala
@@ -0,0 +1,12 @@
+package evo_sim.view.swing.monadic
+
+import cats.effect.IO
+import javax.swing.event.ChangeListener
+import javax.swing.{JComponent, JSlider}
+
+class JSliderIO(jSlider: JSlider) extends JComponentIO(jSlider){
+  def changeListenerAdded(l: ChangeListener) = IO {jSlider.addChangeListener(l)}
+  def changeListenerRemoved(l: ChangeListener) = IO {jSlider.addChangeListener(l)}
+  def minimumSet(min: Int) = IO { jSlider.setMinimum(min) }
+  def maximumSet(max: Int) = IO { jSlider.setMinimum(max) }
+}
