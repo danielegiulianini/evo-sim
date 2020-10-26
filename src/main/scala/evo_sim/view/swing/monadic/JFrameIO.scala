f071ad25ed25e9927b885eafd293ccbd6115a9fe
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-26 12:21:33 +0100

    add defaultCloseOperationSet without invokeAndWait to JFrameIO

--- src/main/scala/evo_sim/view/swing/monadic/JFrameIO.scala
+++ src/main/scala/evo_sim/view/swing/monadic/JFrameIO.scala
@@ -7,16 +7,17 @@ import javax.swing.{JFrame, SwingUtilities, WindowConstants}
 
 
 class JFrameIO(jFrame: JFrame) {
-  //invoke and wait functions (for finer granularity for task assignment to EDT thread)
+  //invoke and wait versions (for finer granularity for task assignment to EDT thread)
   def resizableSetInvokingAndWaiting(frame: JFrame): IO[Unit] = IO { SwingUtilities.invokeAndWait(() => frame.setResizable(false))}
   def visibleSetInvokingAndWaiting(b: Boolean): IO[Unit] = IO{ SwingUtilities.invokeAndWait(() => jFrame.setVisible(b)) }
   def packedInvokingAndWaiting(): IO[Unit] = IO { SwingUtilities.invokeAndWait(() => jFrame.pack()) }
   def defaultCloseOperationSetInvokingAndWaiting(operation:Int): IO[Unit] =
     IO {SwingUtilities.invokeAndWait(() => jFrame.setDefaultCloseOperation(operation))}
-
+  
   def contentPaneGot() = IO { jFrame.getContentPane }
   def sizeSet(width: Int, height: Int) = IO { jFrame.setSize(width, height)}
-  def setLocationRelativeTo(c:Component) = IO {jFrame.setLocationRelativeTo(c)}
+  def locationRelativeToSet(c:Component) = IO {jFrame.setLocationRelativeTo(c)}
+  def defaultCloseOperationSet(operation:Int): IO[Unit] = IO {jFrame.setDefaultCloseOperation(operation)}
 }
 
 //companion object with utilities to be added
