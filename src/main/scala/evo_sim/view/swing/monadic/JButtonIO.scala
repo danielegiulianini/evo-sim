7a396eb0656809ea80edb5b4281dcf87c181fe3e
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-26 14:45:27 +0100

    tweak JButtonIO inheritance

--- src/main/scala/evo_sim/view/swing/monadic/JButtonIO.scala
+++ src/main/scala/evo_sim/view/swing/monadic/JButtonIO.scala
@@ -5,7 +5,7 @@ import java.awt.event.ActionListener
 import cats.effect.IO
 import javax.swing.JButton
 
-class JButtonIO(jButton: JButton) {
+class JButtonIO(jButton: JButton) extends ComponentIO(jButton){
   def actionListenerAdded(l:ActionListener) = IO {jButton.addActionListener(l)}
   def actionListenerRemoved(l:ActionListener) = IO {jButton.removeActionListener(l)}
   def textSet(text: String) = IO {jButton.setText(text)}
@@ -13,3 +13,6 @@ class JButtonIO(jButton: JButton) {
 }
 
 //companion object with utilities to be added
+object JButtonIO {
+  def apply(text:String) = IO { new JButtonIO(new JButton(text)) }
+}
