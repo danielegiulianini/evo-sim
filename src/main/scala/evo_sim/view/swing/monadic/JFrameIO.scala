d4a0bf8c985cfc44d33823acbe276954d1f65234
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-26 12:18:39 +0100

    add locationRelativeToSet to JFrameIO

--- src/main/scala/evo_sim/view/swing/monadic/JFrameIO.scala
+++ src/main/scala/evo_sim/view/swing/monadic/JFrameIO.scala
@@ -1,16 +1,22 @@
 package evo_sim.view.swing.monadic
 
+import java.awt.Component
+
 import cats.effect.IO
 import javax.swing.{JFrame, SwingUtilities, WindowConstants}
 
+
 class JFrameIO(jFrame: JFrame) {
+  //invoke and wait functions (for finer granularity for task assignment to EDT thread)
   def resizableSetInvokingAndWaiting(frame: JFrame): IO[Unit] = IO { SwingUtilities.invokeAndWait(() => frame.setResizable(false))}
   def visibleSetInvokingAndWaiting(b: Boolean): IO[Unit] = IO{ SwingUtilities.invokeAndWait(() => jFrame.setVisible(b)) }
   def packedInvokingAndWaiting(): IO[Unit] = IO { SwingUtilities.invokeAndWait(() => jFrame.pack()) }
-  def contentPaneGot() = IO { jFrame.getContentPane }
   def defaultCloseOperationSetInvokingAndWaiting(operation:Int): IO[Unit] =
     IO {SwingUtilities.invokeAndWait(() => jFrame.setDefaultCloseOperation(operation))}
-  def sizeSet(width: Int, height: Int) = IO {    jFrame.setSize(width, height)  }
+
+  def contentPaneGot() = IO { jFrame.getContentPane }
+  def sizeSet(width: Int, height: Int) = IO { jFrame.setSize(width, height)}
+  def setLocationRelativeTo(c:Component) = IO {jFrame.setLocationRelativeTo(c)}
 }
 
 //companion object with utilities to be added
