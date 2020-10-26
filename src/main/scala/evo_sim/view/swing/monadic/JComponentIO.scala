7ef6bd47d21e3bcc2ce47964b9afec140cad8320
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-26 11:19:04 +0100

    fix JComponentIo name

--- src/main/scala/evo_sim/view/swing/monadic/JComponentIO.scala
+++ src/main/scala/evo_sim/view/swing/monadic/JComponentIO.scala
@@ -5,7 +5,7 @@ import java.awt.Dimension
 import cats.effect.IO
 import javax.swing.JComponent
 
-class IOJComponent[T<:JComponent](jComponent: T) {
+class JComponentIO[T<:JComponent](jComponent: T) {
   def minimumSizeSet(dimension: Dimension): IO[Unit] =
     IO {jComponent.setMinimumSize(dimension)}
   def maximumSizeSet(dimension: Dimension): IO[Unit] =
