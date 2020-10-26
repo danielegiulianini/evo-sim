4095e95e3126aba87c6e50cb2cfc09c505636a03
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-26 11:29:04 +0100

    add BorderSet to JComponentIO

--- src/main/scala/evo_sim/view/swing/monadic/JComponentIO.scala
+++ src/main/scala/evo_sim/view/swing/monadic/JComponentIO.scala
@@ -4,10 +4,15 @@ import java.awt.Dimension
 
 import cats.effect.IO
 import javax.swing.JComponent
+import evo_sim.view.swing.monadic.BorderFactoryIO
+import javax.swing.border.Border
+
 
 class JComponentIO[T<:JComponent](jComponent: T) {
   def minimumSizeSet(dimension: Dimension): IO[Unit] =
     IO {jComponent.setMinimumSize(dimension)}
   def maximumSizeSet(dimension: Dimension): IO[Unit] =
     IO{ jComponent.setMaximumSize(dimension)}
+  def borderSet(border: Border): IO[Unit] =
+    IO {jComponent.setBorder(border)}
 }
