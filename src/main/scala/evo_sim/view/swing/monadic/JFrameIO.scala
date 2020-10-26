9b14d09229c7f831dd65687256ce1cc05463c4cc
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-26 12:13:47 +0100

    add setSize to JFrameIO

--- src/main/scala/evo_sim/view/swing/monadic/JFrameIO.scala
+++ src/main/scala/evo_sim/view/swing/monadic/JFrameIO.scala
@@ -4,12 +4,16 @@ import cats.effect.IO
 import javax.swing.{JFrame, SwingUtilities, WindowConstants}
 
 class JFrameIO(jFrame: JFrame) {
-  def resizedInvokingAndWaiting(frame: JFrame): IO[Unit] = IO { SwingUtilities.invokeAndWait(() => frame.setResizable(false))}
+  def resizableSetInvokingAndWaiting(frame: JFrame): IO[Unit] = IO { SwingUtilities.invokeAndWait(() => frame.setResizable(false))}
   def visibleSetInvokingAndWaiting(b: Boolean): IO[Unit] = IO{ SwingUtilities.invokeAndWait(() => jFrame.setVisible(b)) }
   def packedInvokingAndWaiting(): IO[Unit] = IO { SwingUtilities.invokeAndWait(() => jFrame.pack()) }
   def contentPaneGot() = IO { jFrame.getContentPane }
   def defaultCloseOperationSetInvokingAndWaiting(operation:Int): IO[Unit] =
     IO {SwingUtilities.invokeAndWait(() => jFrame.setDefaultCloseOperation(operation))}
+  def sizeSet(width: Int, height: Int) = IO {    jFrame.setSize(width, height)  }
 }
 
 //companion object with utilities to be added
+object JFrameIO{
+  def apply() = IO { new JFrameIO(new JFrame) }
+}
