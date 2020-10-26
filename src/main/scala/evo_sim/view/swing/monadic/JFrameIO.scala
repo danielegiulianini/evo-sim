0eff9c6185345b75dd7f80adc6236dd72c68326e
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-26 11:35:21 +0100

    add useful methods to JFrameIO

--- src/main/scala/evo_sim/view/swing/monadic/JFrameIO.scala
+++ src/main/scala/evo_sim/view/swing/monadic/JFrameIO.scala
@@ -1,10 +1,13 @@
 package evo_sim.view.swing.monadic
 
 import cats.effect.IO
-import javax.swing.{JFrame, SwingUtilities}
+import javax.swing.{JFrame, SwingUtilities, WindowConstants}
 
 class JFrameIO(jFrame: JFrame) {
   def resizedInvokingAndWaiting(frame: JFrame): IO[Unit] = IO { SwingUtilities.invokeAndWait(() => frame.setResizable(false))}
   def visibleSetInvokingAndWaiting(b: Boolean): IO[Unit] = IO{ SwingUtilities.invokeAndWait(() => jFrame.setVisible(b)) }
   def packedInvokingAndWaiting(): IO[Unit] = IO { SwingUtilities.invokeAndWait(() => jFrame.pack()) }
+  def contentPaneGot() = IO { jFrame.getContentPane }
+  def defaultCloseOperationSetInvokingAndWaiting(operation:Int): IO[Unit] =
+    IO {SwingUtilities.invokeAndWait(() => jFrame.setDefaultCloseOperation(operation))}
 }
