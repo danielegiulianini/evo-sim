a2b4fe91f586a1e75a2420ca21b2d1a3b719aae7
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-28 00:08:48 +0100

    add farenheit syntax for temperature and celsiusToFarenheit conversion

--- src/main/scala/evo_sim/dsl/WorldCreation.scala
+++ src/main/scala/evo_sim/dsl/WorldCreation.scala
@@ -1,13 +1,17 @@
 package evo_sim.dsl
 
 object WorldCreation {
-  implicit class IntWithDegrees(temperature:Int){
-    def °<() = temperature
+  implicit class IntWithDegrees(temperatureInCelsius:Int){
+    lazy val fromCelsiusToFarenheit = 9/5 +32 //should rethink in terms of function
+    def °<() = temperatureInCelsius
+    def F() = temperatureInCelsius * fromCelsiusToFarenheit
   }
 
   implicit class IntWithMeters(lengthInMeters: Int){
+    def m(): Int = lengthInMeters
     def km() = 10 * hm
     def hm(): Int = 100 * lengthInMeters
-    def m(): Int = lengthInMeters
   }
+
+
 }
