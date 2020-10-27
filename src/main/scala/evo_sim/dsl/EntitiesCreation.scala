9a36d75751f20952ef49a23c1517603cb3c6e74a
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-27 16:18:23 +0100

    add explaining comment to EntitiesCreation

--- src/main/scala/evo_sim/dsl/EntitiesCreation.scala
+++ src/main/scala/evo_sim/dsl/EntitiesCreation.scala
@@ -1,7 +1,18 @@
 package evo_sim.dsl
 
 object EntitiesCreation {
+  /*This method enables syntax like this:
 
+val baseBlobs = 15 of BaseBlob(
+... blob properties...
+)
+
+    instead of:
+
+val baseBlobs = Iterator.tabulate(15)(i => BaseBlob(
+... blob properties...
+)
+  )*/
 
   implicit class FromIntToList(int:Int) {
     def of[T](t: =>T) = Iterator.fill(int)(t).toSet
