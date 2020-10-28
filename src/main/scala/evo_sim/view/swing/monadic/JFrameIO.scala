49bec3354952f5dd612490caf0d5107661acd5dd
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-28 11:24:56 +0100

    add JFrameIO's methods version with invokeAndWait for thread-safety

--- src/main/scala/evo_sim/view/swing/monadic/JFrameIO.scala
+++ src/main/scala/evo_sim/view/swing/monadic/JFrameIO.scala
@@ -7,18 +7,21 @@ import javax.swing.{JFrame, SwingUtilities, WindowConstants}
 
 
 class JFrameIO(val jFrame: JFrame) extends ContainerIO(jFrame) {
-  //invoke and wait versions (for finer granularity for task assignment to EDT thread)
-  def resizableInvokingAndWaiting(resizable: Boolean): IO[Unit] = IO { SwingUtilities.invokeAndWait(() => jFrame.setResizable(resizable))}
-  def visibleInvokingAndWaiting(b: Boolean): IO[Unit] = IO{ SwingUtilities.invokeAndWait(() => jFrame.setVisible(b)) }
-  def packedInvokingAndWaiting(): IO[Unit] = IO { SwingUtilities.invokeAndWait(() => jFrame.pack()) }
-  def defaultCloseOperationSetInvokingAndWaiting(operation:Int): IO[Unit] =
-    IO {SwingUtilities.invokeAndWait(() => jFrame.setDefaultCloseOperation(operation))}
-
   def contentPane() = IO { new ContainerIO (jFrame.getContentPane) }
   def sizeSet(width: Int, height: Int) = IO { jFrame.setSize(width, height)}
   def locationRelativeToSet(c:Component) = IO {jFrame.setLocationRelativeTo(c)}
   def defaultCloseOperationSet(operation:Int): IO[Unit] = IO {jFrame.setDefaultCloseOperation(operation)}
   def titleSet(title: String) = IO{jFrame.setTitle(title)}
+
+  //invoke and wait versions (for finer granularity for task assignment to EDT thread)
+  def resizableInvokingAndWaiting(resizable: Boolean): IO[Unit] = IO { SwingUtilities.invokeAndWait(() => jFrame.setResizable(resizable))}
+  def visibleInvokingAndWaiting(b: Boolean): IO[Unit] = IO{ SwingUtilities.invokeAndWait(() => jFrame.setVisible(b)) }
+  def packedInvokingAndWaiting(): IO[Unit] = IO { SwingUtilities.invokeAndWait(() => jFrame.pack()) }
+  def contentPaneInvokingAndWaiting() = IO { SwingUtilities.invokeAndWait( () => new ContainerIO (jFrame.getContentPane)) }
+  def sizeSetInvokingAndWaiting(width: Int, height: Int) = IO { SwingUtilities.invokeAndWait( () => jFrame.setSize(width, height))}
+  def locationRelativeToSetInvokingAndWaiting(c:Component) = IO {SwingUtilities.invokeAndWait( () => jFrame.setLocationRelativeTo(c))}
+  def defaultCloseOperationSetInvokingAndWaiting(operation:Int): IO[Unit] = IO {SwingUtilities.invokeAndWait( ()=> jFrame.setDefaultCloseOperation(operation))}
+  def titleSetInvokingAndWaiting(title: String) = IO{SwingUtilities.invokeAndWait( () => jFrame.setTitle(title))}
 }
 
 //companion object with utilities
