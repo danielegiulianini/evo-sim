2bed106d615bb0347f1d7ade6e307b51deb0587f
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-28 11:45:55 +0100

    tweak indentation in ContainerIO

--- src/main/scala/evo_sim/view/swing/monadic/ContainerIO.scala
+++ src/main/scala/evo_sim/view/swing/monadic/ContainerIO.scala
@@ -5,26 +5,12 @@ import java.awt.{Component, Container, LayoutManager}
 import cats.effect.IO
 
 class ContainerIO[T<:Container](val container: Container) extends ComponentIO(container) {
-  def added(component: ComponentIO[_<:Component]) = IO {
-    container.add(component.component)
-  }
-  def added(name: String, component: ComponentIO[ _<:Component]) = IO {
-    container.add(name, component.component)
-  }
-  def added(component: ComponentIO[ _<:Component], constraints : Object) = IO {
-    container.add(component.component, constraints)
-  }
-  def removed(component: ComponentIO[ _<:Component]) = IO {
-    container.remove(component.component)
-  }
-  def allRemoved() = IO {
-    container.removeAll()
-  }
-  def layoutSet(mgr : LayoutManager): IO[Unit] = IO {
-    container.setLayout(mgr)
-  }
-
-
+  def added(component: ComponentIO[_<:Component]) = IO {container.add(component.component)}
+  def added(name: String, component: ComponentIO[ _<:Component]) = IO {    container.add(name, component.component)}
+  def added(component: ComponentIO[ _<:Component], constraints : Object) = IO {    container.add(component.component, constraints)}
+  def removed(component: ComponentIO[ _<:Component]) = IO {    container.remove(component.component)  }
+  def allRemoved() = IO {    container.removeAll()  }
+  def layoutSet(mgr : LayoutManager): IO[Unit] = IO {    container.setLayout(mgr)  }
 }
 
 //companion object with utilities to be added
