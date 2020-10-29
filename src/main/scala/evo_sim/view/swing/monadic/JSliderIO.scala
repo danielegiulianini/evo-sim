c98d68d3f20cc0b535c4bef8ba59cd5711597847
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-29 22:57:07 +0100

    add changeListenerAdded with listener monadic specification

--- src/main/scala/evo_sim/view/swing/monadic/JSliderIO.scala
+++ src/main/scala/evo_sim/view/swing/monadic/JSliderIO.scala
@@ -1,7 +1,7 @@
 package evo_sim.view.swing.monadic
 
 import cats.effect.IO
-import javax.swing.event.ChangeListener
+import javax.swing.event.{ChangeEvent, ChangeListener}
 import javax.swing.{JComponent, JSlider}
 
 class JSliderIO(override val component: JSlider) extends JComponentIO(component){
@@ -15,6 +15,12 @@ class JSliderIO(override val component: JSlider) extends JComponentIO(component)
   def minorTickSpacingSet(spacing: Int): IO[Unit] = IO { component.setMinorTickSpacing(spacing) }
   def paintTicksSet(b: Boolean): IO[Unit] = IO { component.setPaintTicks(b) }
   def paintLabelsSet(b: Boolean): IO[Unit] = IO { component.setPaintLabels(b) }
+
+  def actionListenerAdded(l:ChangeEvent => IO[Unit]) =
+    IO {component.addChangeListener( e => l(e).unsafeRunSync() )}
+  //event listener that doesn't leverage action event parameter
+  def actionListenerAdded(l: => IO[Unit]) =
+    IO {component.addChangeListener( _ => l.unsafeRunSync() )}
 }
 
 
