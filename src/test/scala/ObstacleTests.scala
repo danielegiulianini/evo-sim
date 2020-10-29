c1385fbaf8dc0553e51388fbcfbf1d8ffde7f7b6
alessandro-oliva4 <alessandro.oliva4@studio.unibo.it>
2020-10-29 23:07:18 +0100

    refactor entity tests

--- src/test/scala/ObstacleTests.scala
+++ src/test/scala/ObstacleTests.scala
@@ -2,11 +2,10 @@ import evo_sim.model.Entities.{BaseBlob, BaseObstacle}
 import evo_sim.model.EntityStructure.Blob
 import evo_sim.model.Utils.chain
 import evo_sim.model._
-import org.scalatest.FunSuite
+import org.scalatest.FunSpec
 
-class ObstacleTests extends FunSuite {
-
-  val blob: BaseBlob = BaseBlob(
+class ObstacleTests extends FunSpec {
+  private val blob: BaseBlob = BaseBlob(
     name = "blob",
     boundingBox = BoundingBox.Circle(point = Point2D(100, 100), radius = 10),
     life = Constants.DEF_BLOB_LIFE,
@@ -15,32 +14,71 @@ class ObstacleTests extends FunSuite {
     fieldOfViewRadius = 10,
     movementStrategy = MovingStrategies.baseMovement,
     direction = Direction(0, 15))
-
-  val mud: BaseObstacle = BaseObstacle(
+  private val puddle: BaseObstacle = BaseObstacle(
     name = "mud",
     boundingBox = BoundingBox.Rectangle(point = Point2D(100, 100), width = 50, height = 40),
     effect = Effect.slowEffect)
-
-  val stone: BaseObstacle = BaseObstacle(
+  private val stone: BaseObstacle = BaseObstacle(
     name = "stone",
     boundingBox = BoundingBox.Rectangle(point = Point2D(100, 100), width = 50, height = 40),
     effect = Effect.damageEffect)
+  private val world: World = new World(
+    temperature = Constants.DEF_TEMPERATURE,
+    luminosity = Constants.DEFAULT_LUMINOSITY,
+    width = Constants.WORLD_WIDTH,
+    height = Constants.WORLD_HEIGHT,
+    currentIteration = 0,
+    entities = Set(blob) ++ Set(puddle) ++ Set(stone),
+    totalIterations = 10
+  )
 
-  test("BlobSlowed") {
-    val updatedBlob = blob.collided(mud).toVector(0).asInstanceOf[Blob]
-    assert(updatedBlob.velocity == 50)
+  describe("A BaseObstacle") {
+    describe("with mud effect") {
+      describe("after an update") {
+        it("should never change") {
+          assert(Set(puddle) == puddle.updated(world))
+        }
+      }
+      describe("after a collision") {
+        it("should never change") {
+          assert(Set(puddle) == puddle.collided(blob))
+        }
+      }
+    }
+    describe("with damage effect") {
+      describe("after an update") {
+        it("should never change") {
+          assert(Set(stone) == stone.updated(world))
+        }
+      }
+      describe("after a collision") {
+        it("should never change") {
+          assert(Set(stone) == stone.collided(blob))
+        }
+      }
+    }
   }
 
-  test("BlobDamaged") {
-    val updatedBlob = blob.collided(stone).toVector(0).asInstanceOf[Blob]
-    assert(updatedBlob.life == (Constants.DEF_BLOB_LIFE - Constants.DEF_DAMAGE))
+  describe("A BaseBlob") {
+    describe("when colliding with a puddle") {
+      it("should decrease its velocity") {
+        val updatedBlob = blob.collided(puddle).toVector(0).asInstanceOf[Blob]
+        assert(updatedBlob.velocity == 50)
+      }
+    }
+    describe("when colliding with a stone") {
+      it("should decrease its energy") {
+        val updatedBlob = blob.collided(stone).toVector(0).asInstanceOf[Blob]
+        assert(updatedBlob.life == (Constants.DEF_BLOB_LIFE - Constants.DEF_DAMAGE))
+      }
+      describe("multiple times") {
+        it("should die") {
+          val updatedBlob = chain(40)(blob)(b =>
+            b.collided(stone).toVector(0).asInstanceOf[BaseBlob])
+          assert(updatedBlob.life <= 0)
+        }
+      }
+    }
   }
-
-  test("BlobKilled") {
-    val updatedBlob = chain(40)(blob)(b =>
-      b.collided(stone).toVector(0).asInstanceOf[BaseBlob])
-    assert(updatedBlob.life == 0)
-  }
-
 }
 
--- src/test/scala/PlantTests.scala
+++ src/test/scala/PlantTests.scala
@@ -5,29 +5,27 @@ import evo_sim.model.Constants._
 import org.scalatest.FunSuite
 
 class PlantTests extends FunSuite {
-
-  val plant = StandardPlant(
+  private val plant = StandardPlant(
     name = "standardPlant1",
     boundingBox = BoundingBox.Rectangle(point = World.randomPosition(), width = DEF_STANDARD_PLANT_WIDTH, height = DEF_STANDARD_PLANT_HEIGHT),
     lifeCycle = DEF_LIFECYCLE)
-  val plant2 = StandardPlant(
+  private val plant2 = StandardPlant(
     name = "standardPlant2",
     boundingBox = BoundingBox.Rectangle(point = World.randomPosition(), width = DEF_STANDARD_PLANT_WIDTH, height = DEF_STANDARD_PLANT_HEIGHT),
     lifeCycle = 0)
-  val world: World = World.apply(temperature = DEF_TEMPERATURE, luminosity = DEFAULT_LUMINOSITY, width = WORLD_WIDTH, height = WORLD_HEIGHT,
+  private val world: World = World.apply(temperature = DEF_TEMPERATURE, luminosity = DEFAULT_LUMINOSITY, width = WORLD_WIDTH, height = WORLD_HEIGHT,
     currentIteration = 0, entities = Set(plant, plant2), totalIterations = DEF_DAYS * ITERATIONS_PER_DAY)
 
   test("updatedPlant") {
     val updatedPlant = plant.updated(world)
     val updatedPlant2 = plant2.updated(world)
-    assert(updatedPlant.exists(e => e match {
-      case p : StandardPlant => p.lifeCycle == plant.lifeCycle - 1
+    assert(updatedPlant.exists {
+      case p: StandardPlant => p.lifeCycle == plant.lifeCycle - 1
       case _ => false
-    }))
-    assert(updatedPlant2.exists(e => e match {
-      case p : StandardPlant => p.lifeCycle == DEF_LIFECYCLE
+    })
+    assert(updatedPlant2.exists {
+      case p: StandardPlant => p.lifeCycle == DEF_LIFECYCLE
       case _ => false
-    }))
+    })
   }
-
 }
