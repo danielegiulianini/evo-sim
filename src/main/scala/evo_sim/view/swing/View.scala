5b7306ebfa560c2d0c9db7d095152ba5d6a80f10
alessandro-oliva4 <alessandro.oliva4@studio.unibo.it>
2020-10-29 22:19:53 +0100

    Add event listener description by monad for sliders + refactor listener description to monads for all buttons and sliders

--- src/main/scala/evo_sim/view/swing/View.scala
+++ src/main/scala/evo_sim/view/swing/View.scala
@@ -1,22 +1,15 @@
 package evo_sim.view.swing
 
-import java.awt.{BorderLayout, Container, Dimension, Toolkit}
-import java.awt.event.ActionEvent
+import java.awt.{BorderLayout, Dimension, Toolkit}
 
 import cats.effect.IO
-import evo_sim.model.Constants.ITERATIONS_PER_DAY
 import evo_sim.model.World.fromIterationsToDays
-import evo_sim.model.{Constants, Environment, World}
+import evo_sim.model.{Environment, World}
 import evo_sim.view.View
-import evo_sim.view.swing.chart.JFreeChart.{JFreeChart, XYSeries, XYSeriesCollection}
-import evo_sim.view.swing.chart.View.{setContentPane, setDefaultCloseOperation, setSize, setVisible}
+import evo_sim.view.swing.effects.InputViewEffects.inputViewCreated
 import evo_sim.view.swing.custom.components.ShapesPanel
-import evo_sim.view.swing.effects.InputViewEffects._
-import evo_sim.view.swing.monadic.{JButtonIO, JFrameIO, JLabelIO, JPanelIO}
+import evo_sim.view.swing.monadic.{JFrameIO, JLabelIO, JPanelIO}
 import javax.swing._
-import org.jfree.chart.ChartFactory
-import org.jfree.ui.tabbedui.VerticalLayout
-import org.knowm.xchart.{XChartPanel, XYChartBuilder}
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Promise}
@@ -26,46 +19,9 @@ object View extends View {
   val frame = new JFrameIO(new JFrame("evo-sim"))
 
   override def inputReadFromUser(): IO[Environment] = for {
-    environmentPromise <- IO pure {
-      Promise[Environment]()
-    }
-    inputPanel <- JPanelIO()
-    _ <- inputPanel.layoutSet(new VerticalLayout())
-    blobSlider <- createDataInputRow(inputPanel, "#Blob", Constants.MIN_BLOBS, Constants.MAX_BLOBS,
-      Constants.DEF_BLOBS)
-    plantSlider <- createDataInputRow(inputPanel, "#Plant", Constants.MIN_PLANTS, Constants.MAX_PLANTS,
-      Constants.DEF_PLANTS)
-    obstacleSlider <- createDataInputRow(inputPanel, "#Obstacle", Constants.MIN_OBSTACLES,
-      Constants.MAX_OBSTACLES, Constants.DEF_OBSTACLES)
-    luminositySlider <- createDataInputRow(inputPanel, "Luminosity (cd)",
-      Constants.SELECTABLE_MIN_LUMINOSITY, Constants.SELECTABLE_MAX_LUMINOSITY, Constants.DEFAULT_LUMINOSITY)
-    temperatureSlider <- createDataInputRow(inputPanel, "Temperature (°C)",
-      Constants.SELECTABLE_MIN_TEMPERATURE, Constants.SELECTABLE_MAX_TEMPERATURE, Constants.DEF_TEMPERATURE)
-    daysSlider <- createDataInputRow(inputPanel, "#Days", Constants.MIN_DAYS, Constants.MAX_DAYS,
-      Constants.DEF_DAYS)
-    start <- JButtonIO("Start")
-    _ <- start.actionListenerAdded((_: ActionEvent) => (for {
-      _ <- start.enabledSet(false)
-      t <- temperatureSlider.valueGot
-      l <- luminositySlider.valueGot
-      b <- blobSlider.valueGot
-      p <- plantSlider.valueGot
-      o <- obstacleSlider.valueGot
-      d <- daysSlider.valueGot
-      _ <- IO {
-        environmentPromise.success(Environment(t, l, b, p, o, d))
-      }
-    } yield ()).unsafeRunSync)
-    cp <- frame.contentPane()
-    _ <- cp.added(inputPanel, BorderLayout.CENTER)
-    _ <- cp.added(start, BorderLayout.SOUTH)
-    _ <- frame.defaultCloseOperationSet(WindowConstants.EXIT_ON_CLOSE)
-    _ <- frame.packedInvokingAndWaiting()
-    _ <- frame.resizableInvokingAndWaiting(false)
-    _ <- frame.visibleInvokingAndWaiting(true)
-    environment <- IO {
-      Await.result(environmentPromise.future, Duration.Inf)
-    }
+    environmentPromise <- IO pure { Promise[Environment]() }
+    _ <- inputViewCreated(frame, environmentPromise)
+    environment <- IO { Await.result(environmentPromise.future, Duration.Inf) }
   } yield environment
 
   override def rendered(world: World): IO[Unit] = for {
--- src/main/scala/evo_sim/view/swing/monadic/JSliderIO.scala
+++ src/main/scala/evo_sim/view/swing/monadic/JSliderIO.scala
@@ -1,7 +1,9 @@
 package evo_sim.view.swing.monadic
 
+import java.awt.event.ActionEvent
+
 import cats.effect.IO
-import javax.swing.event.ChangeListener
+import javax.swing.event.{ChangeEvent, ChangeListener}
 import javax.swing.{JComponent, JSlider}
 
 class JSliderIO(override val component: JSlider) extends JComponentIO(component){
@@ -15,6 +17,12 @@ class JSliderIO(override val component: JSlider) extends JComponentIO(component)
   def minorTickSpacingSet(spacing: Int): IO[Unit] = IO { component.setMinorTickSpacing(spacing) }
   def paintTicksSet(b: Boolean): IO[Unit] = IO { component.setPaintTicks(b) }
   def paintLabelsSet(b: Boolean): IO[Unit] = IO { component.setPaintLabels(b) }
+
+  //enabling event listener description by monad
+  def changeListenerAdded(l: ChangeEvent => IO[Unit]): IO[Unit] =
+    IO { component.addChangeListener(e => l(e).unsafeRunSync()) }
+  def changeListenerAdded(l: => IO[Unit]): IO[Unit] =
+    IO { component.addChangeListener(_ => l.unsafeRunSync()) }
 }
 
 
