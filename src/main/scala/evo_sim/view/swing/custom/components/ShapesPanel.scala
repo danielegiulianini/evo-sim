eef13a2e3d9758cd21771ce3da3e0b79ac4810bf
Andrea Betti <andrea.betti10@studio.unibo.it>
2020-10-29 19:22:46 +0100

    Scaladoc and refactoring for ShapesPanel + delete Reproducible

--- src/main/scala/evo_sim/view/swing/custom/components/ShapesPanel.scala
+++ src/main/scala/evo_sim/view/swing/custom/components/ShapesPanel.scala
@@ -9,6 +9,11 @@ import evo_sim.model.EntityStructure.{Blob, Entity, Obstacle}
 import evo_sim.model.{Intersection, Point2D, World}
 import javax.swing.JPanel
 
+/**
+ * JPanel implementation that visualizes world entities in a bidimensional space.
+ *
+ * @param world World to represent
+ */
 class ShapesPanel(world: World) extends JPanel {
 
   override def paintComponent(g: Graphics): Unit = {
@@ -22,8 +27,7 @@ class ShapesPanel(world: World) extends JPanel {
     g.fillRect(0, 0, getWidth, getHeight)
 
     // draw rectangles and triangles before the transparent filter and circles
-    world.entities.foreach(e =>
-      drawFoodsObstaclesAndPlants(g, e, getWidth, getHeight, world.width, world.height))
+    world.entities.foreach(e => drawFoodObstacleOrPlant(g, e))
 
     // draw luminosity filter
     val alpha = 255 - modelToViewRatio(world.luminosity, 255, MAX_LUMINOSITY).max(0).min(255)
@@ -32,7 +36,7 @@ class ShapesPanel(world: World) extends JPanel {
     g.fillRect(0, 0, getWidth, getHeight)
 
     // draw blob circles with the field of view
-    drawBlobs(g, world, getWidth, getHeight)
+    drawBlobs(g)
   }
 
   override def getPreferredSize: Dimension = {
@@ -40,22 +44,34 @@ class ShapesPanel(world: World) extends JPanel {
     new Dimension(getParent.getSize().width - borderValue, getParent.getSize().height - borderValue)
   }
 
-  private def drawFieldOfView(g: Graphics, b: Blob, world: World, viewWidth: Int, viewHeight: Int): Unit = {
+  /**
+   * Draws field of view of a blob as a circle and interesected entities.
+   *
+   * @param g Graphics used by paintComponent method
+   * @param b Blob used to represent his field of view
+   * @return a set with the blob produced by the effect
+   */
+  private def drawFieldOfView(g: Graphics, b: Blob): Unit = {
     g.setColor(new Color(255, 255, 0))
-    val x = modelToViewRatio(b.boundingBox.point.x - b.fieldOfViewRadius, viewWidth, world.width)
-    val y = modelToViewRatio(b.boundingBox.point.y - b.fieldOfViewRadius, viewHeight, world.height)
-    val width = modelToViewRatio(b.fieldOfViewRadius * 2, viewWidth, world.width)
-    val height = modelToViewRatio(b.fieldOfViewRadius * 2, viewHeight, world.height)
+    val x = modelToViewRatio(b.boundingBox.point.x - b.fieldOfViewRadius, getWidth, world.width)
+    val y = modelToViewRatio(b.boundingBox.point.y - b.fieldOfViewRadius, getHeight, world.height)
+    val width = modelToViewRatio(b.fieldOfViewRadius * 2, getWidth, world.width)
+    val height = modelToViewRatio(b.fieldOfViewRadius * 2, getHeight, world.height)
     g.drawOval(x, y, width, height)
     world.entities.filter(e2 => Intersection.intersected(Circle(b.boundingBox.point,
-      b.fieldOfViewRadius), e2.boundingBox)).foreach(e2 =>
-      drawFoodsObstaclesAndPlants(g, e2, getWidth, getHeight, world.width, world.height))
+      b.fieldOfViewRadius), e2.boundingBox)).foreach(e2 => drawFoodObstacleOrPlant(g, e2))
   }
 
-  private def drawBlobs(g: Graphics, world: World, viewWidth: Int, viewHeight: Int): Unit = {
+  /**
+   * Draws blobs as circles.
+   *
+   * @param g Graphics used by paintComponent method
+   */
+  private def drawBlobs(g: Graphics): Unit = {
     world.entities.foreach(e => {
+      // draw field of view for each blob
       e match {
-        case b: Blob => drawFieldOfView(g, b, world, viewWidth, viewHeight)
+        case b: Blob => drawFieldOfView(g, b)
         case _ =>
       }
       e.boundingBox match {
@@ -67,18 +83,23 @@ class ShapesPanel(world: World) extends JPanel {
             case _: PoisonBlob => g.setColor(Color.MAGENTA)
             case _ => g.setColor(Color.BLACK)
           }
-          val x = modelToViewRatio(point2D.x - r, viewWidth, world.width)
-          val y = modelToViewRatio(point2D.y - r, viewHeight, world.height)
-          val width = modelToViewRatio(r * 2, viewWidth, world.width)
-          val height = modelToViewRatio(r * 2, viewHeight, world.height)
-          g.fillOval(x, y, width, height)
+          val x = modelToViewRatio(point2D.x - r, getWidth, world.width)
+          val y = modelToViewRatio(point2D.y - r, getHeight, world.height)
+          val ovalWidth = modelToViewRatio(r * 2, getWidth, world.width)
+          val ovalHeight = modelToViewRatio(r * 2, getHeight, world.height)
+          g.fillOval(x, y, ovalWidth, ovalHeight)
         case _ =>
       }
     })
   }
 
-  private def drawFoodsObstaclesAndPlants(g: Graphics, entity: Entity, viewWidth: Int, viewHeight: Int,
-                                          worldWidth: Int, worldHeight: Int): Unit = {
+  /**
+   * Draws a entity with triangle or rectangle bounding box (foods, obstacles and plants).
+   *
+   * @param g Graphics used by paintComponent method
+   * @param entity Entity to represent
+   */
+  private def drawFoodObstacleOrPlant(g: Graphics, entity: Entity): Unit = {
     entity.boundingBox match {
       case Rectangle(point2D, w, h) =>
         entity match {
@@ -88,21 +109,21 @@ class ShapesPanel(world: World) extends JPanel {
           case _: PoisonousPlant => g.setColor(Color.MAGENTA)
           case _ => g.setColor(Color.black)
         }
-        val x = modelToViewRatio(point2D.x - w / 2, viewWidth, viewWidth)
-        val y = modelToViewRatio(point2D.y - h / 2, viewHeight, viewHeight)
-        val width = modelToViewRatio(w, viewWidth, worldWidth)
-        val height = modelToViewRatio(h, viewHeight, worldHeight)
-        g.fillRect(x, y, width, height)
+        val x = modelToViewRatio(point2D.x - w / 2, getWidth, world.width)
+        val y = modelToViewRatio(point2D.y - h / 2, getHeight, world.height)
+        val rectWidth = modelToViewRatio(w, getWidth, world.width)
+        val rectHeight = modelToViewRatio(h, getHeight, world.height)
+        g.fillRect(x, y, rectWidth, rectHeight)
       case Triangle(point2D, h, a) =>
         val vertices = triangleVertices(Triangle(point2D, h, a))
         g.setColor(Color.green)
         val xPoints = vertices.productIterator.map({
           case p: Point2D =>
-            modelToViewRatio(p.x, viewWidth, worldWidth)
+            modelToViewRatio(p.x, getWidth, world.width)
         }).toArray
         val yPoints = vertices.productIterator.map({
           case p: Point2D =>
-            modelToViewRatio(p.y, viewHeight, worldHeight)
+            modelToViewRatio(p.y, getHeight, world.height)
         }).toArray
         val nPoints = vertices.productIterator.length
         g.fillPolygon(xPoints, yPoints, nPoints)
@@ -110,10 +131,13 @@ class ShapesPanel(world: World) extends JPanel {
     }
   }
 
-  /** Return a tuple3 with the vertices
+  /** Returns a tuple3 with the vertices
    * v1 = center.x, center.y  + radius           -> upper vertices
    * v2 = center.x - radius, center.y  - radius  -> bottom left vertices
    * v1 = center.x + radius, center.y  - radius  -> bottom right vertices
+   *
+   * @param tri Triangle bounding box
+   * @return triangle vertices
    */
   private def triangleVertices(tri: Triangle): (Point2D, Point2D, Point2D) = {
     val radius = tri.height / 3 * 2
@@ -121,6 +145,13 @@ class ShapesPanel(world: World) extends JPanel {
       Point2D(tri.point.x + radius, tri.point.y - radius))
   }
 
+  /** Proportional mapping of a model property from model to view dimensions
+   *
+   * @param modelProperty A model property
+   * @param viewDimension Maximum view dimension
+   * @param modelDimension Maximum model dimension
+   * @return Equivalent value of model property in view space
+   */
   private def modelToViewRatio(modelProperty: Double, viewDimension: Double, modelDimension: Double): Int =
     (modelProperty * viewDimension / modelDimension).round.toInt
 
