24f0ab2d5a43abd6c5f98dd913b754206651cb5e
Andrea Vaienti <andrea.vaienti2@studio.unibo.it>
2020-10-30 12:07:34 +0100

    Refactoring movement strategies

--- src/main/scala/evo_sim/model/MovingStrategies.scala
+++ src/main/scala/evo_sim/model/MovingStrategies.scala
@@ -11,6 +11,7 @@ case class Direction(angle: Int, stepToNextDirection: Int)
 
 object MovingStrategies {
 
+  /**  */
   def baseMovement(entity: Intelligent, world: World, entitiesFilter: Entity => Boolean): Movement = {
     val chasedEntity = (world.entities - entity.asInstanceOf[SimulableEntity]).filter(entitiesFilter) match {
       case set if set.nonEmpty => Option(set.minBy(elem => distanceBetweenEntities(entity.boundingBox.point, elem.boundingBox.point)))
@@ -28,3 +29,24 @@ object MovingStrategies {
   }
 
 }
+
+/*
+chart <- IO { new XYChart(200,200)}
+    _ <- IO { chart.addSeries("prova", List.range(0,10).map(_.toDouble).toArray) }
+
+    chartPanel <- IO { new JComponentIO(new XChartPanel[XYChart](chart)) }
+    chartPanel2 <- IO { new JComponentIO(new XChartPanel[XYChart](chart)) }
+    _ <- IO { println(chartPanel.component.getWidth) }
+
+    /* panel <- JPanelIO()
+    _ <- IO { panel.layoutSet(new VerticalLayout()) }
+    _ <- IO { panel.added(chartPanel) }
+    _ <- IO { panel.added(chartPanel2) } */
+
+    cp <- frame.contentPane()
+    _ <- cp.allRemovedInvokingAndWaiting()
+    _ <- cp.addedInvokingAndWaiting(chartPanel2, BorderLayout.NORTH)
+    _ <- cp.addedInvokingAndWaiting(chartPanel, BorderLayout.CENTER)
+    _ <- frame.packedInvokingAndWaiting()
+    _ <- frame.visibleInvokingAndWaiting(true)
+ */
