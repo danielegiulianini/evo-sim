961048ea61666fe473d0669e3a4a2466fd075281
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-10-31 22:23:38 +0100

    add scaladoc to conversions between iterations and days

--- src/main/scala/evo_sim/model/World.scala
+++ src/main/scala/evo_sim/model/World.scala
@@ -123,10 +123,24 @@ object World {
   }
 
 
-
-
+  /**
+   * Converts from simulation iterations (the atomic temporal unit that regulates the
+   * simulation) to days (the temporal interval between which [[EnvironmentParameters]]
+   * return the same), depending on [[ITERATIONS_PER_DAY]].
+   * @param iteration iteration
+   * @return the days amount corresponding to the iteration
+   */
   def fromIterationsToDays(iteration : Int) = iteration / ITERATIONS_PER_DAY
-  def fromDaysToIterations(days : Int) = days * ITERATIONS_PER_DAY
+
+  /**
+   * Converts from simulation days (the temporal interval between which [[EnvironmentParameters]]
+   * return the same) and iterations (the atomic temporal unit that regulates the
+   * simulation) to iterations, depending on [[ITERATIONS_PER_DAY]].
+   *
+   * @param day day
+   * @return the iterations amount corresponding to the days
+   */
+  def fromDaysToIterations(day : Int) = day * ITERATIONS_PER_DAY
 
 
   /** Returns the time of the day as a float ranging from 0 to 1 excluded given the iteration number.
@@ -138,6 +152,8 @@ object World {
   def timeOfTheDay(iteration: Int): Float =
     iteration % Constants.ITERATIONS_PER_DAY / Constants.ITERATIONS_PER_DAY.toFloat
 }
+
+
 
 /*
 def apply(env: Environment): World = {
