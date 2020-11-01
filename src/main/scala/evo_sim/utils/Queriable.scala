13058fcb735098a83cff0bce97c4c1bbaf2a1342
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-11-01 14:31:21 +0100

    complete scaladoc for Queriable

--- src/main/scala/evo_sim/utils/Queriable.scala
+++ src/main/scala/evo_sim/utils/Queriable.scala
@@ -3,7 +3,7 @@ package evo_sim.utils
 /** A type class that represents the ability for a type functor to be queried, i.e. asked whether it contains
  * a given element.
  *
- * @tparam F the type functor to be enriched with the [[Queriable]]#contained function
+ * @tparam F the type functor to be enriched with the [[Queriable]]#contained function.
  */
 trait Queriable[F[_]]{
   /**
@@ -22,13 +22,11 @@ trait Queriable[F[_]]{
  */
 object Queriable {
 
-  /**
+  /** Returns if the given element is contained inside the structure provided.
    *
-   * @param t
-   * @param elem
-   * @tparam F
-   * @tparam A
-   * @return
+   * @param t the type functor instance that wants to be inspected for the presence of elem
+   * @param elem the element to be searched for
+   * @return whether the type functor contains the element.
    */
   def containedAnyOf[F[_]: Queriable, A](t: F[A], elem: (A, A)): Boolean =
     implicitly[Queriable[F]].contained(t, elem._1) || implicitly[Queriable[F]].contained(t, elem._2)
@@ -40,18 +38,29 @@ object Queriable {
   }*/
 }
 
-/** Provides some implicit conversions for [[evo_sim.utils.Queriable]] type class.
+/** Provides some implicit implementations of [[evo_sim.utils.Queriable]] type class.
  */
 object QueriableImplicits {
+
+  /** Implicit implementation of [[evo_sim.utils.Queriable]] for [[Set]], ie: how it can be
+   * considered a [[Queriable]].
+   */
   implicit object ContainsForSet extends Queriable[Set]{
     override def contained[T](t: Set[T], elem: T): Boolean = t.contains(elem)
   }
 
+
+  /** Implicit implementation of [[evo_sim.utils.Queriable]] for [[HomogeneousTuple2]], ie: how it can be
+   * considered a [[Queriable]].
+   */
    implicit object ContainsForHomogeneousTuple2 extends Queriable[({type HomogeneousTuple2[A] = (A, A)})#HomogeneousTuple2]{
      override def contained[T](t: (T, T), elem: T): Boolean =
        t._1 == elem || t._2 == elem
    }
 
+  /** Implicit implementation of [[evo_sim.utils.Queriable]] for [[HomogeneousTuple2Set]], ie: how it can be
+   * considered a [[Queriable]].
+   */
    implicit object ContainsForHomogeneousTuple2Set extends Queriable[({type HomogeneousTuple2Set[A] = Set[(A, A)]})#HomogeneousTuple2Set]{
      override def contained[T](t: Set[(T, T)], elem: T): Boolean = {
        t.exists(QueriableImplicits.ContainsForHomogeneousTuple2.contained(_, elem))
