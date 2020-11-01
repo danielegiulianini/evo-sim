07e4ee2320758f176dfbe6bf4f52f186ba20c1b4
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-11-01 22:54:55 +0100

    complete scaladoc for trigonometricalOps

--- src/main/scala/evo_sim/utils/TrigonometricalOps.scala
+++ src/main/scala/evo_sim/utils/TrigonometricalOps.scala
@@ -24,10 +24,28 @@ object TrigonometricalOps {
     def sinusoidal(yDilatation: Float)(x: Float)(phase: Int)(yTranslation: Int): Int =
       (yDilatation * Math.sin(2 * Math.PI * x + phase)).toInt + yTranslation //should rename yDilatation to amplitude
 
+    /**
+     * Computes the [[sinusoidal]] function value with the input provided and a phase equals to 0.
+     * If
+     * @return
+     */
     def zeroPhasedSinusoidal: (Float, Float, Int) => Int = sinusoidal(_: Float)(_: Float)(0)(_: Int)
 
+    /**
+     * Computes the [[sinusoidal]] function value with the input provided and a yTranslation equals to 0.
+     * Everytime [[sinusoidal]] function would be called with a yTranslation equals to 0, this function should be
+     * invoked instead.
+     * @return the value corresponding to input provided.
+     */
     def zeroYTranslatedSinusoidal: (Float, Float, Int) => Int = sinusoidal(_: Float)(_: Float)(_: Int)(0)
 
+    /**
+     * Computes the [[sinusoidal]] function value with the input provided and a phase equals to 0 and a yTranslation
+     * equals to 0.
+     * Everytime [[sinusoidal]] function would be called with a phase equals to 0 and a yTranslation equals to 0,
+     * this function should be invoked instead.
+     * @return the value corresponding to input provided.
+     */
     def zeroPhasedZeroYTranslatedSinusoidal: (Float, Float) => Int = Curried.zeroPhasedSinusoidal(_: Float)(_: Float)(0)
 
     /**
