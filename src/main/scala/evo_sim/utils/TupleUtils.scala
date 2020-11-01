26c7f2e3e577790ddd55341ccc7b0f96eecc3774
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-11-01 18:46:31 +0100

    add scaladoc to HomogeneousTuple2Set

--- src/main/scala/evo_sim/utils/TupleUtils.scala
+++ src/main/scala/evo_sim/utils/TupleUtils.scala
@@ -9,13 +9,26 @@ import evo_sim.utils.TupleUtils.Tuple2Types.HomogeneousTuple2Set
 
 object TupleUtils {
 
-  /**
-   *
-   */
+  /** An object containing some [[Tuple2]]-related types aliases. */
   object Tuple2Types {
+
+    /** An homogeneous [[Tuple2]], i.e. a Tuple2 with 2 elements of the same generic type.
+     * @tparam A the type of the element contained inside the the Tuple2.
+     */
     type HomogeneousTuple2[A] = (A, A)
+
+    /** A [[Set]] of [[HomogeneousTuple2]]s.
+     * @tparam A
+     */
     type HomogeneousTuple2Set[A] = Set[(A, A)]
+
+    /** A companion object that acts as a factory for HomogeneousTuple2Sets.
+     */
     object HomogeneousTuple2Set {
+      /**
+       * Build an empty HomogeneousTuple2Set instance of the specified type.
+       * @tparam A the generic type of HomogeneousTuple2Set to build
+       */
       def apply[A](): HomogeneousTuple2Set[A] = Set()
     }
   }
@@ -26,7 +39,11 @@ object TupleUtils {
    */
   def toTuple2[A](a: A): (A, A) = (a, a)
 
-
+  /**
+   * @param mySet
+   * @tparam T
+   * @return
+   */
   def everyElementPairedWithOnlyOneOtherElement[T](mySet: HomogeneousTuple2Set[T]): HomogeneousTuple2Set[T] =
     mySet.foldLeft(HomogeneousTuple2Set[T]())(
       (acc, t) =>
