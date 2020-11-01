947ab1bbce6389fb8a72cb544160145a0f01a1aa
Andrea Betti <andrea.betti10@studio.unibo.it>
2020-11-01 02:12:24 +0100

    Move chain in test utils + test packaging

---
+++ src/test/scala/evo_sim/model/TupleUtilsTests.scala
@@ -0,0 +1,14 @@
+/*import evo_sim.core.TupleUtils
+import evo_sim.core.TupleUtils.{containedAnyOf, everyElementPairedWithOnlyOneOtherElement}
+
+class TestTupleUtils {
+  //test
+  val testSet: Set[(Int,Int)]= Set((1, 2), (1, 3))
+  val tupleSet = (0, 2)
+  println("starting set:" + testSet)
+  println("the set " + testSet +" contains any of: " + tupleSet + "?:" + containedAnyOf(testSet, tupleSet))
+  println("resulting is :" + everyElementPairedWithOnlyOneOtherElement(testSet))
+
+  val testSet2 = Set((1, 2), (1, 3), (3, 1))
+  println("il set filtrato e': " + TupleUtils.everyElementPairedWithOnlyOneOtherElement(testSet2))
+}*/
