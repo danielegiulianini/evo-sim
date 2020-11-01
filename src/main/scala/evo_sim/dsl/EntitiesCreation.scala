6dd57be302397028cd3e21d94bc17887e03a02d8
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-11-01 17:35:51 +0100

    add scaladoc to dsl

--- src/main/scala/evo_sim/dsl/EntitiesCreation.scala
+++ src/main/scala/evo_sim/dsl/EntitiesCreation.scala
@@ -1,20 +1,32 @@
 package evo_sim.dsl
 
 object EntitiesCreation {
-  /*This method enables syntax like this:
 
-val baseBlobs = 15 of BaseBlob(
-... blob properties...
-)
-
-    instead of:
-
-val baseBlobs = Iterator.tabulate(15)(i => BaseBlob(
-... blob properties...
-)
-  )*/
-
+  /**
+   * Implicit class that enables rich syntax on Int instances.
+   */
   implicit class FromIntToList(int:Int) {
+
+    /**
+     * This method enables a more declarative and concise syntax for a Set creation.
+     * Using it in Entity creation results in this syntax:
+     *
+     * {{{
+     * val baseBlobs = 15 of BaseBlob(
+     * ... blob properties...
+     * )
+     * }}}
+     * Instead of:
+     * {{{
+     * val baseBlobs = Iterator.tabulate(15)(i => BaseBlob(
+     * ... blob properties...
+     * )
+     * }}}
+     *
+     * @param t the [[Function0]] used to fill the Set to be returned.
+     * @tparam T the type of the elements added to the Set to be returned.
+     * @return a Set of int elements of type T
+     */
     def of[T](t: =>T) = Iterator.fill(int)(t).toSet
   }
 }
--- src/main/scala/evo_sim/dsl/SimulationStart.scala
+++ src/main/scala/evo_sim/dsl/SimulationStart.scala
@@ -4,7 +4,7 @@ import evo_sim.core.Simulation.SimulationIO
 
 object SimulationStart {
 
-  /** prettier method name than "unsafeRunAsync for starting simulation"*/
+  /** Enables prettier method name than "unsafeRunAsync" for starting simulation.*/
   implicit class SimulationCanStart[A](simulation: SimulationIO[A]) {
     def run(): A = simulation unsafeRunSync
   }
