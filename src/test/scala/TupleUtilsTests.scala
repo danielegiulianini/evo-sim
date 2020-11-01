7072c5a06930213e26f7f1d121d3d1a3cb44bd4d
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-11-01 10:54:15 +0100

    uodate specification for TupleUtilsTest

--- src/test/scala/TupleUtilsTests.scala
+++ src/test/scala/TupleUtilsTests.scala
@@ -1,14 +1,30 @@
-/*import evo_sim.core.TupleUtils
-import evo_sim.core.TupleUtils.{containedAnyOf, everyElementPairedWithOnlyOneOtherElement}
+import org.scalatest.FunSpec
 
-class TestTupleUtils {
+class TestTupleUtils extends FunSpec {
+
+  //to be tested:
+  //1. everyElementPairedWithOnlyOneOtherElement
+
+  //2. implicit conversions:
+    //1. contained for tuples
+    //2. contained for set
+    //3. contained for tupleset
+
+  //3.containsAnyOf
+
   //test
-  val testSet: Set[(Int,Int)]= Set((1, 2), (1, 3))
+  val testSet: Set[(Int,Int)] = Set((1, 2), (1, 3))
   val tupleSet = (0, 2)
-  println("starting set:" + testSet)
+
+
+  /*println("starting set:" + testSet)
   println("the set " + testSet +" contains any of: " + tupleSet + "?:" + containedAnyOf(testSet, tupleSet))
   println("resulting is :" + everyElementPairedWithOnlyOneOtherElement(testSet))
 
   val testSet2 = Set((1, 2), (1, 3), (3, 1))
-  println("il set filtrato e': " + TupleUtils.everyElementPairedWithOnlyOneOtherElement(testSet2))
-}*/
+  println("il set filtrato e': " + TupleUtils.everyElementPairedWithOnlyOneOtherElement(testSet2))*/
+
+  describe("A ") {
+
+  }
+}
