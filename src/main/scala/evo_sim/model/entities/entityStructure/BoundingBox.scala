ea432cbd13e21236b995f9cdc20f059db051d4bc
unknown <rei.beshiri@studio.unibo.it>
2020-11-02 23:36:58 +0100

    scala doc fix

--- src/main/scala/evo_sim/model/entities/entityStructure/BoundingBox.scala
+++ src/main/scala/evo_sim/model/entities/entityStructure/BoundingBox.scala
@@ -38,7 +38,7 @@ object BoundingBox {
   //case classes for the different types of Bounding Boxes
   //Circle: Point & Radius
   /**
-   * Circle Shape, used by [[evo_sim.model.EntityStructure.Blob]].
+   * Circle Shape, used by [[evo_sim.model.entities.entityStructure.EntityStructure.Blob]].
    *
    * @param point  a [[evo_sim.model.entities.entityStructure.Point2D]] represent the center of the Circle.
    * @param radius represent the radius of the Circle.
@@ -47,7 +47,7 @@ object BoundingBox {
 
   //Rectangle: Point & w + h
   /**
-   * Rectangle Shape, used by [[evo_sim.model.EntityStructure.Obstacle]].
+   * Rectangle Shape, used by [[evo_sim.model.entities.entityStructure.EntityStructure.Obstacle]].
    *
    * @param point  a [[evo_sim.model.entities.entityStructure.Point2D]] represent the center of the Rectangle.
    * @param width  represent the width of the Rectangle.
@@ -57,7 +57,7 @@ object BoundingBox {
 
   //Triangle: Point & h + angle (angle is defaulted as 60 -> equilateral), apothem = h/3*2 -> circe radius circumscribed in the triangle
   /**
-   * Triangle Shape, used by [[evo_sim.model.EntityStructure.Food]].
+   * Triangle Shape, used by [[evo_sim.model.entities.entityStructure.EntityStructure.Food]].
    *
    * @param point  a [[evo_sim.model.entities.entityStructure.Point2D]] represent the center of the Triangle.
    * @param height represent the height of the Triangle.
