178897ff49ae06b29bb3c70b6a741d89e06fb7e9
unknown <rei.beshiri@studio.unibo.it>
2020-11-02 23:34:27 +0100

    remove useless comment

--- src/main/scala/evo_sim/model/entities/entityBehaviour/Collidable.scala
+++ src/main/scala/evo_sim/model/entities/entityBehaviour/Collidable.scala
@@ -38,30 +38,4 @@ object Collidable {
     self: Entity with Updatable =>
     override def collided(other: SimulableEntity): Set[SimulableEntity] = Set(self)
   }
-
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-/**
- * collided(blob, blob)
- * collided(blob, food)
- * collided(blob, obstacle)
- * -> note, every collided with the specific type of entity, this permits to define the effect occurred to the blob
- *
-def collided(other: Blob) : Set[Simulable] = {
-    val set = Set(other)
-    set -> this cant be done since Blob does not extend Simulable (so does not extends Collidable and Updatable either)
-  }
- */
