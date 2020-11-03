4f9892a9af41918657fc3b37782e089faa957a76
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-11-03 22:05:35 +0100

    add scaladoc to swing.monadic

--- src/main/scala/evo_sim/view/swing/monadic/package.scala
+++ src/main/scala/evo_sim/view/swing/monadic/package.scala
@@ -3,6 +3,10 @@ package evo_sim.view.swing
 import cats.effect.IO
 import javax.swing.SwingUtilities
 
+/** A monadic wrapper for Swing and Awt main components that provides an access point to the Swing APIs with
+ * a monadic taste. It is independent from the simulator domain and can be potentially re-used outside this
+ * project.
+ */
 package object monadic {
   def invokeAndWaitIO(runnable : => Unit): IO[Unit] = IO {SwingUtilities.invokeAndWait(() => runnable)}
   def invokeLaterIO(runnable : => Unit): IO[Unit] = IO {SwingUtilities.invokeLater(() => runnable)}
