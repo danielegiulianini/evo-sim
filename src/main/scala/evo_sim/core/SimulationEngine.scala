9931dd9b14018c3a2341f0974bbfbcee8719632f
alessandro-oliva4 <alessandro.oliva4@studio.unibo.it>
2020-11-03 21:08:50 +0100

    Add CLI user guide in report

--- src/main/scala/evo_sim/core/SimulationEngine.scala
+++ src/main/scala/evo_sim/core/SimulationEngine.scala
@@ -5,8 +5,8 @@ import cats.effect.IO
 import evo_sim.core.Simulation.toStateTConversions._
 import evo_sim.model.world.Constants.SIMULATION_LOOP_PERIOD
 import evo_sim.model.world.World
-import evo_sim.view.swing.SwingView.inputReadFromUser
-//import evo_sim.view.cli.CLIView.inputReadFromUser
+//import evo_sim.view.swing.SwingView.inputReadFromUser
+import evo_sim.view.cli.CLIView.inputReadFromUser
 import evo_sim.core.Simulation._
 
 import scala.concurrent.duration._
