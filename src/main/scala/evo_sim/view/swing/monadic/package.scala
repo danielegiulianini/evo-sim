cfab8154019d1a75605c798c92780d196a0b1a18
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-11-30 00:37:56 +0100

    add scaladoc to invokingAndWaiting

--- src/main/scala/evo_sim/view/swing/monadic/package.scala
+++ src/main/scala/evo_sim/view/swing/monadic/package.scala
@@ -13,22 +13,24 @@ import javax.swing.SwingUtilities
  */
 package object monadic {
   /**
-   * Returns the [[IO]] containing the code for synchronously executing the computation description inside
-   * [[computation]] on the AWT event dispatching thread, in a thread-safe manner.
+   * Returns the [[IO]] containing the code for synchronously executing the computation described inside the
+   * computation parameter on the AWT event dispatching thread, in a thread-safe manner.
+   * This method should be used whenever a thread other than EDT needs to update the GUI.
+   * Unlike [[invokingLater]], upon calling "unsafeRunSync" on the IO returned, the control flow
+   * will block until all pending AWT events have been processed and only then the
+   * [[SwingUtilities#invokeLater]] wrapped call will return.
+   * If searching for an asynchronous execution of [[IO]]'s content, must use [[invokingLater]] instead.
+   *
    * @see [[evo_sim.view.swing.monadic.ExampleWithLayoutWithSwingMonadic]] for examples of how to use it
-   * *         inside a monadic chain.
-   * @param computation
-   * @return
+   *          inside a monadic chain.
+   * @param computation the [[IO]] containing the GUI-related logic to be executed.
+   * @return an IO wrapping the [[SwingUtilities#invokeAndWait]] thread-safe call.
    */
   def invokingAndWaiting(computation: IO[_]): IO[Unit] = IO {
     SwingUtilities.invokeAndWait(() => computation.unsafeRunSync())
   }
 
-  /**
-   *
-   * @param computation
-   * @return
-   */
+ 
   def invokingLater(computation: IO[_]): IO[Unit] = IO {
     SwingUtilities.invokeLater(() => computation.unsafeRunSync())
   }
