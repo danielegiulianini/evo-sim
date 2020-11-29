03bf3eaf573e36f0099a4a2b57a2af1faf3d5a48
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-11-29 23:13:58 +0100

    update scaladoc for ComponentIO

--- src/main/scala/evo_sim/view/swing/monadic/ComponentIO.scala
+++ src/main/scala/evo_sim/view/swing/monadic/ComponentIO.scala
@@ -5,8 +5,7 @@ import java.awt.event.{ComponentAdapter, ComponentEvent, ComponentListener, Mous
 
 import cats.effect.IO
 
-/**
- * A class that provides a monadic description of the operations supplied by awt's [[Component]] in the form
+/** A class that provides a monadic description of the operations supplied by awt's [[Component]] in the form
  * of IO monad in a purely functional fashion.
  * Every Swing's Component could be wrapped by this class, but note that this package provided some ad-hoc factory
  * utilities for the most popular Swing's components (see [[JPanelIO]], [[JFrameIO]], [[JButtonIO]]).
@@ -14,14 +13,37 @@ import cats.effect.IO
  * @tparam T the type of the component to be wrapped whose methods are to be enhanced with [[IO]] description.
  */
 class ComponentIO[T<:Component](val component: T){
-  def componentListenerAdded(l: ComponentListener): IO[Unit] = IO { component.addComponentListener(l) }
+  //TODO procedural vs monadic for listeners
+  /** Returns an [[IO]] containing the description of a [[java.awt.Component#addMouseListener]]
+   * method invocation.*/
   def mouseListenerAdded(l:MouseListener): IO[Unit] = IO {component.addMouseListener(l) }
+
+  /** Returns an [[IO]] containing the description of a [[java.awt.Component#removeMouseListener]]
+   * method invocation.*/
   def mouseListenerRemoved(l:MouseListener): Unit = IO { component.removeMouseListener(l) }
-  def fontGot(): IO[Font] = IO {component.getFont}
-  def setPreferredSize(d: Dimension): IO[Unit] = IO {component.setPreferredSize(d) }
+
+  /** Returns an [[IO]] containing the description of a [[java.awt.Component#addComponentListener]]
+   * method invocation. */
+  def componentListenerAdded(l: ComponentListener): IO[Unit] = IO { component.addComponentListener(l) }
+
+  /** Returns an [[IO]] containing the code for registering a [[ComponentAdapter]] listener for handling
+   * component events from this component*/
   def componentAdapterAdded(): IO[Unit] = IO {
     component.addComponentListener(new ComponentAdapter {
       override def componentResized(e: ComponentEvent): Unit =
         component.setPreferredSize(component.getSize)
     })}
+
+  /** Returns an [[IO]] containing the description of a [[java.awt.Component#removeComponentListener]]
+   * method invocation. */
+  def componentListenerRemoved(l: ComponentListener): IO[Unit] = IO { component.removeComponentListener(l) }
+
+  /** Returns an [[IO]] containing the description of a [[java.awt.Component#getFont]]
+   * method invocation. */
+  def fontGot(): IO[Font] = IO {component.getFont}
+
+  /** Returns an [[IO]] containing the description of a [[java.awt.Component#setPreferredSize]]
+   * method invocation. */
+  def setPreferredSize(d: Dimension): IO[Unit] = IO {component.setPreferredSize(d) }
+
 }
