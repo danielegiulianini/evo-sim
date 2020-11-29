5f5b46a066542d742a8b00fc1af4003a024f7751
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-11-30 00:01:50 +0100

    update scaladoc of JFrameIO

--- src/main/scala/evo_sim/view/swing/monadic/JFrameIO.scala
+++ src/main/scala/evo_sim/view/swing/monadic/JFrameIO.scala
@@ -12,15 +12,41 @@ import javax.swing.JFrame
  */
 class JFrameIO(override val component: JFrame) extends ContainerIO(component) {
 
+  /** Returns an [[IO]] containing the description of a [[JFrame#getContentPane]]
+   * method invocation.*/
   def contentPane(): IO[ContainerIO[Container]] = IO { new ContainerIO (component.getContentPane) }
+
+  /** Returns an [[IO]] containing the description of a [[JFrame#setResizable]]
+   * method invocation.*/
   def resizableSet(resizable: Boolean): IO[Unit] = IO{component.setResizable(resizable)}
+
+  /** Returns an [[IO]] containing the description of a [[JFrame#setVisible]]
+   * method invocation.*/
   def visibleSet(b: Boolean): IO[Unit] = IO{component.setVisible(b)}
+
+  /** Returns an [[IO]] containing the description of a [[JFrame#pack]]
+   * method invocation.*/
   def packed(): IO[Unit] = IO{component.pack()}
+
+  /** Returns an [[IO]] containing the description of a [[JFrame#setSize]]
+   * method invocation.*/
   def sizeSet(width: Int, height: Int): IO[Unit] = IO { component.setSize(width, height)}
+
+  /** Returns an [[IO]] containing the description of a [[JFrame#setLocationRelativeTo]]
+   * method invocation.*/
   def locationRelativeToSet(c:Component): IO[Unit] = IO {component.setLocationRelativeTo(c)}
+
+  /** Returns an [[IO]] containing the description of a [[JFrame#setDefaultCloseOperation]]
+   * method invocation.*/
   def defaultCloseOperationSet(operation:Int): IO[Unit] = IO {component.setDefaultCloseOperation(operation)}
+
+  /** Returns an [[IO]] containing the description of a [[JFrame#setTitle]]
+   * method invocation.*/
   def titleSet(title: String): IO[Unit] = IO{component.setTitle(title)}
-  def setMaximizedExtendedState(): IO[Unit] = IO{component.setExtendedState(component.getExtendedState | Frame.MAXIMIZED_BOTH)}
+
+  /** Returns an [[IO]] containing the code for maximizing the state of the JFrame wrapped by this instance.*/
+  def setMaximizedExtendedState(): IO[Unit] =
+    IO{component.setExtendedState(component.getExtendedState | Frame.MAXIMIZED_BOTH)}
 }
 
 /** A factory for [[IO]]s containing a JFrameIO instance.*/
