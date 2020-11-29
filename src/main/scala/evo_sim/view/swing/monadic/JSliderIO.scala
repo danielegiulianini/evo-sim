b37d60e09312da3a6580960a4fad0b22370287a8
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-11-30 00:04:24 +0100

    update scaladoc of JSliderIO

--- src/main/scala/evo_sim/view/swing/monadic/JSliderIO.scala
+++ src/main/scala/evo_sim/view/swing/monadic/JSliderIO.scala
@@ -9,13 +9,37 @@ import javax.swing.JSlider
  * @param component the jSlider that this class wraps.
  */
 class JSliderIO(override val component: JSlider) extends JComponentIO(component){
+
+  /** Returns an [[IO]] containing the description of a [[JSlider#setMinimum]]
+   * method invocation on the [[JSlider]] wrapped in this instance. */
   def minimumSet(min: Int): IO[Unit] = IO { component.setMinimum(min) }
+
+  /** Returns an [[IO]] containing the description of a [[JSlider#setMaximum]]
+   * method invocation on the [[JSlider]] wrapped in this instance. */
   def maximumSet(max: Int): IO[Unit] = IO { component.setMaximum(max) }
+
+  /** Returns an [[IO]] containing the description of a [[JSlider#setValue]]
+   * method invocation on the [[JSlider]] wrapped in this instance. */
   def valueSet(value: Int): IO[Unit] = IO { component.setValue(value) }
+
+  /** Returns an [[IO]] containing the description of a [[JSlider#getValue]]
+   * method invocation on the [[JSlider]] wrapped in this instance. */
   def valueGot: IO[Int] = IO { component.getValue }
+
+  /** Returns an [[IO]] containing the description of a [[JSlider#setMajorTickSpacing]]
+   * method invocation on the [[JSlider]] wrapped in this instance. */
   def majorTickSpacingSet(spacing: Int): IO[Unit] = IO { component.setMajorTickSpacing(spacing) }
+
+  /** Returns an [[IO]] containing the description of a [[JSlider#setMinorTickSpacing]]
+   * method invocation on the [[JSlider]] wrapped in this instance. */
   def minorTickSpacingSet(spacing: Int): IO[Unit] = IO { component.setMinorTickSpacing(spacing) }
+
+  /** Returns an [[IO]] containing the description of a [[JSlider#setPaintTicks]]
+   * method invocation on the [[JSlider]] wrapped in this instance. */
   def paintTicksSet(b: Boolean): IO[Unit] = IO { component.setPaintTicks(b) }
+
+  /** Returns an [[IO]] containing the description of a [[JSlider#setPaintLabels]]
+   * method invocation on the [[JSlider]] wrapped in this instance. */
   def paintLabelsSet(b: Boolean): IO[Unit] = IO { component.setPaintLabels(b) }
 
   //procedural event listener description:
